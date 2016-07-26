009f9b0c0421b471b67805ec0a57f3f9b7435640
Ondrej Ruttkay <ondrej@automattic.com>
2016-07-26 15:28:19 +0200

    Update hidden tags test example

--- app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
+++ app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
@@ -24,8 +24,19 @@ class MainActivity : Activity() {
         private val LINK = "<a href=\"https://github.com/wordpress-mobile/WordPress-Aztec-Android\">Link</a><br><br>"
         private val UNKNOWN = "<iframe class=\"classic\">Menu</iframe><br><br>"
         private val COMMENT = "<!--This is a comment--><br><br>"
-        private val HIDDEN = "<span></span><div class=\"first\"><div class=\"second\">" +
-                "<div class=\"third\">Div<br><span><b>Span</b></span>Hidden</div><div class=\"fourth\"></div></div></div><br><br>"
+        private val HIDDEN = 
+                "<span></span>" +
+                "<div class=\"first\">" +
+                "    <div class=\"second\">" +
+                "        <div class=\"third\">" +
+                "            Div<br><span><b>Span</b></span><br>Hidden" +
+                "        </div>" +
+                "        <div class=\"fourth\"></div>" +
+                "        <div class=\"fifth\"></div>" +
+                "    </div>" +
+                "    <span class=\"second last\"></span>" +
+                "</div>" +
+                "<br><br>"
         private val EXAMPLE = BOLD + ITALIC + UNDERLINE + STRIKETHROUGH + BULLET + QUOTE + LINK + UNKNOWN + COMMENT + HIDDEN
     }
 

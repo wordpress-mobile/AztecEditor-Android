30fae6758b2739927382ff7dda841eb29f7f38cd
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2016-07-27 21:08:58 +0900

    New url handling dialog and logic (add/change).

--- app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
+++ app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
@@ -1,10 +1,14 @@
 package org.wordpress.aztec.demo
 
-import android.app.Activity
 import android.app.AlertDialog
+import android.content.ClipboardManager
+import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
+import android.text.style.URLSpan
+import android.util.Patterns
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
@@ -16,7 +20,7 @@ import org.wordpress.aztec.toolbar.ToolbarAction
 import org.wordpress.aztec.toolbar.ToolbarActionType
 import java.util.*
 
-class MainActivity : Activity(), FormatToolbar.OnToolbarActionListener {
+class MainActivity : AppCompatActivity(), FormatToolbar.OnToolbarActionListener {
 
     companion object {
         private val BOLD = "<b>Bold</b><br><br>"
@@ -32,7 +36,7 @@ class MainActivity : Activity(), FormatToolbar.OnToolbarActionListener {
     }
 
     private lateinit var aztec: AztecText
-    private lateinit var mToolbar: FormatToolbar
+    private lateinit var mFormattingToolbar: FormatToolbar
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,25 +44,13 @@ class MainActivity : Activity(), FormatToolbar.OnToolbarActionListener {
         setContentView(R.layout.activity_main)
 
         aztec = findViewById(R.id.aztec) as AztecText
-        mToolbar = findViewById(R.id.toolbar) as FormatToolbar
+        mFormattingToolbar = findViewById(R.id.formatting_toolbar) as FormatToolbar
 
 
         //highlight toolbar buttons based on what styles are applied to the text beneath cursor
         aztec.setOnSelectionChangedListener(object : AztecText.OnSelectionChangedListener {
             override fun onSelectionChanged(selStart: Int, selEnd: Int) {
-                var newSelStart = selStart
-
-                if (selStart > 0 && !aztec.isTextSelected()) {
-                    newSelStart = selStart - 1
-                }
-
-                val appliedStyles = aztec.getAppliedStyles(newSelStart, selEnd)
-
-                //remember the last active style after emptying EditText manually
-                if (!aztec.isEmpty()) {
-                    aztec.setSelectedStyles(appliedStyles)
-                    mToolbar.highlightActionButtons(ToolbarAction.getToolbarActionsForStyles(appliedStyles))
-                }
+                highlightAppliedStyles(selStart,selEnd)
             }
         })
 
@@ -66,13 +58,30 @@ class MainActivity : Activity(), FormatToolbar.OnToolbarActionListener {
 //        aztec.fromHtml(EXAMPLE)
         aztec.setSelection(aztec.editableText.length)
 
-        mToolbar.setToolbarActionListener(this)
+        mFormattingToolbar.setToolbarActionListener(this)
+    }
+
+
+    fun highlightAppliedStyles(selStart: Int, selEnd: Int){
+        var newSelStart = selStart
+
+        if (selStart > 0 && !aztec.isTextSelected()) {
+            newSelStart = selStart - 1
+        }
+
+        val appliedStyles = aztec.getAppliedStyles(newSelStart, selEnd)
+
+        //remember the last active style after emptying EditText manually
+        if (!aztec.isEmpty()) {
+            aztec.setSelectedStyles(appliedStyles)
+            mFormattingToolbar.highlightActionButtons(ToolbarAction.getToolbarActionsForStyles(appliedStyles))
+        }
     }
 
     override fun onToolbarAction(action: ToolbarAction) {
         //if noting is selected just activate style
         if (!aztec.isTextSelected() && action.actionType == ToolbarActionType.INLINE_STYLE) {
-            val actions = mToolbar.getSelectedActions()
+            val actions = mFormattingToolbar.getSelectedActions()
             val textFormats = ArrayList<TextFormat>()
 
             actions.forEach { if (it.isStylingAction()) textFormats.add(it.textFormat!!) }
@@ -99,22 +108,79 @@ class MainActivity : Activity(), FormatToolbar.OnToolbarActionListener {
         val start = aztec.selectionStart
         val end = aztec.selectionEnd
 
+        val urlSpans = aztec.text.getSpans(start, end, URLSpan::class.java)
+
+
+        val url: String?
+        var anchor: String?
+
+        var modifyingExistingLink = false
+
+        var spanStart: Int = 0
+        var spanEnd: Int = 0
+
+        if (!urlSpans.isEmpty()) {
+            val urlSpan = urlSpans[0]
+
+            spanStart = aztec.text.getSpanStart(urlSpan)
+            spanEnd = aztec.text.getSpanEnd(urlSpan)
+
+            anchor = aztec.text.substring(spanStart, spanEnd)
+            url = urlSpan.url
+
+            if(anchor.equals(url)){
+                anchor = ""
+            }
+
+            modifyingExistingLink = true
+
+        } else {
+
+            val clipboardUrl = getUrlFromClipboard(this)
+
+            if(TextUtils.isEmpty(clipboardUrl)){
+                url = ""
+            }else{
+                url = clipboardUrl
+            }
+
+
+
+            if (start == end) {
+                anchor = ""
+            } else {
+                anchor = aztec.text.substring(start, end)
+            }
+        }
+
+
         val builder = AlertDialog.Builder(this)
         builder.setCancelable(false)
 
         val view = layoutInflater.inflate(R.layout.dialog_link, null, false)
-        val editText = view.findViewById(R.id.edit) as EditText
+        val urlInput = view.findViewById(R.id.linkURL) as EditText
+        val anchorInput = view.findViewById(R.id.linkText) as EditText
+
+        urlInput.setText(url)
+        anchorInput.setText(anchor)
+
         builder.setView(view)
         builder.setTitle(R.string.dialog_title)
 
         builder.setPositiveButton(R.string.dialog_button_ok, DialogInterface.OnClickListener { dialog, which ->
-            val link = editText.text.toString().trim { it <= ' ' }
+            val link = urlInput.text.toString().trim { it <= ' ' }
+            val anchorText = anchorInput.text.toString().trim { it <= ' ' }
+
             if (TextUtils.isEmpty(link)) {
                 return@OnClickListener
             }
 
-            // When AztecText lose focus, use this method
-            aztec.link(link, start, end)
+            if (modifyingExistingLink) {
+                aztec.editLink(link, anchorText, spanStart, spanEnd)
+            } else {
+                aztec.addLink(link, anchorText, start, end)
+            }
+
         })
         builder.create().show()
     }
@@ -133,5 +199,20 @@ class MainActivity : Activity(), FormatToolbar.OnToolbarActionListener {
         }
 
         return true
+    }
+
+
+    /**
+     * Checks the Clipboard for text that matches the [Patterns.WEB_URL] pattern.
+
+     * @return the URL text in the clipboard, if it exists; otherwise null
+     */
+    fun getUrlFromClipboard(context: Context?): String? {
+        if (context == null) return null
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val data = clipboard?.primaryClip
+        if (data == null || data.itemCount <= 0) return null
+        val clipText = data.getItemAt(0).text.toString()
+        return if (Patterns.WEB_URL.matcher(clipText).matches()) clipText else null
     }
 }

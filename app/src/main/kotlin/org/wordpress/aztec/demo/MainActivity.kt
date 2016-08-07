6978dac9d2dfe724abb827b5691be9c6f4bc8603
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2016-08-07 21:03:11 +0900

    Encapsulated toolbar logic.

--- app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
+++ app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
@@ -1,26 +1,13 @@
 package org.wordpress.aztec.demo
 
-import android.app.AlertDialog
-import android.content.ClipboardManager
-import android.content.Context
-import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.text.TextUtils
-import android.text.style.URLSpan
-import android.util.Patterns
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.EditText
-import android.widget.Toast
 import org.wordpress.aztec.AztecText
-import org.wordpress.aztec.TextFormat
 import org.wordpress.aztec.toolbar.FormatToolbar
-import org.wordpress.aztec.toolbar.ToolbarAction
-import org.wordpress.aztec.toolbar.ToolbarActionType
-import java.util.*
 
-class MainActivity : AppCompatActivity(), FormatToolbar.OnToolbarActionListener {
+class MainActivity : AppCompatActivity() {
 
     companion object {
         private val BOLD = "<b>Bold</b><br><br>"
@@ -32,25 +19,27 @@ class MainActivity : AppCompatActivity(), FormatToolbar.OnToolbarActionListener 
         private val LINK = "<a href=\"https://github.com/wordpress-mobile/WordPress-Aztec-Android\">Link</a><br><br>"
         private val UNKNOWN = "<iframe class=\"classic\">Menu</iframe><br><br>"
         private val COMMENT = "<!--This is a comment--><br><br>"
-        private val HIDDEN = 
+        private val HIDDEN =
                 "<span></span>" +
-                "<div class=\"first\">" +
-                "    <div class=\"second\">" +
-                "        <div class=\"third\">" +
-                "            Div<br><span><b>Span</b></span><br>Hidden" +
-                "        </div>" +
-                "        <div class=\"fourth\"></div>" +
-                "        <div class=\"fifth\"></div>" +
-                "    </div>" +
-                "    <span class=\"second last\"></span>" +
-                "</div>" +
-                "<br><br>"
+                        "<div class=\"first\">" +
+                        "    <div class=\"second\">" +
+                        "        <div class=\"third\">" +
+                        "            Div<br><span><b>Span</b></span><br>Hidden" +
+                        "        </div>" +
+                        "        <div class=\"fourth\"></div>" +
+                        "        <div class=\"fifth\"></div>" +
+                        "    </div>" +
+                        "    <span class=\"second last\"></span>" +
+                        "</div>" +
+                        "<br><br>"
         private val EXAMPLE = BOLD + ITALIC + UNDERLINE + STRIKETHROUGH + BULLET + QUOTE + LINK + UNKNOWN + COMMENT + HIDDEN
     }
 
     private lateinit var aztec: AztecText
     private lateinit var mFormattingToolbar: FormatToolbar
 
+    private val TEST = "<ul><li>one</li><li>hello</li><li>two</li></ul>"
+//    private val TEST = "<blockquote>one</blockquote>two<blockquote>three</blockquote>"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,155 +47,13 @@ class MainActivity : AppCompatActivity(), FormatToolbar.OnToolbarActionListener 
 
         aztec = findViewById(R.id.aztec) as AztecText
         mFormattingToolbar = findViewById(R.id.formatting_toolbar) as FormatToolbar
+        mFormattingToolbar.setEditor(aztec, "")
 
 
-        //highlight toolbar buttons based on what styles are applied to the text beneath cursor
-        aztec.setOnSelectionChangedListener(object : AztecText.OnSelectionChangedListener {
-            override fun onSelectionChanged(selStart: Int, selEnd: Int) {
-                highlightAppliedStyles(selStart,selEnd)
-            }
-        })
-
-        // ImageGetter coming soon...
-//        aztec.fromHtml(EXAMPLE)
+//        aztec.fromHtml(TEST)
         aztec.setSelection(aztec.editableText.length)
-
-        mFormattingToolbar.setToolbarActionListener(this)
     }
 
-
-    fun highlightAppliedStyles(selStart: Int, selEnd: Int){
-        var newSelStart = selStart
-
-        if (selStart > 0 && !aztec.isTextSelected()) {
-            newSelStart = selStart - 1
-        }
-
-        val appliedStyles = aztec.getAppliedStyles(newSelStart, selEnd)
-
-        //remember the last active style after emptying EditText manually
-        if (!aztec.isEmpty()) {
-            aztec.setSelectedStyles(appliedStyles)
-            mFormattingToolbar.highlightActionButtons(ToolbarAction.getToolbarActionsForStyles(appliedStyles))
-        }
-    }
-
-    override fun onToolbarAction(action: ToolbarAction) {
-        //if noting is selected just activate style
-        if (!aztec.isTextSelected() && action.actionType == ToolbarActionType.INLINE_STYLE) {
-            val actions = mFormattingToolbar.getSelectedActions()
-            val textFormats = ArrayList<TextFormat>()
-
-            actions.forEach { if (it.isStylingAction()) textFormats.add(it.textFormat!!) }
-            return aztec.setSelectedStyles(textFormats)
-        }
-
-        //if text is selected and action is styling toggle it's style
-        if (action.isStylingAction()) {
-            return aztec.toggleFormatting(action.textFormat!!)
-        }
-
-        //other toolbar action
-        when (action) {
-            ToolbarAction.LINK -> showLinkDialog()
-            ToolbarAction.HTML -> aztec.setText(aztec.toHtml())
-            else -> {
-                Toast.makeText(this@MainActivity, "Unsupported action", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-
-    private fun showLinkDialog() {
-        var start = aztec.selectionStart
-        var end = aztec.selectionEnd
-
-        val urlSpans = aztec.text.getSpans(start, end, URLSpan::class.java)
-
-
-        val url: String?
-        var anchor: String?
-
-        var modifyingExistingLink = false
-
-        var spanStart: Int = 0
-        var spanEnd: Int = 0
-
-        if (!urlSpans.isEmpty()) {
-            val urlSpan = urlSpans[0]
-
-            spanStart = aztec.text.getSpanStart(urlSpan)
-            spanEnd = aztec.text.getSpanEnd(urlSpan)
-
-            if(start < spanStart || end > spanEnd){
-                //looks like some text that is not part of the url was included in selection
-                anchor = aztec.text.substring(start, end)
-                url = ""
-
-            }else{
-                anchor = aztec.text.substring(spanStart, spanEnd)
-                url = urlSpan.url
-                start = spanStart
-                end = spanEnd
-            }
-
-            if(anchor.equals(url)){
-                anchor = ""
-            }
-
-
-            modifyingExistingLink = true
-
-        } else {
-
-            val clipboardUrl = getUrlFromClipboard(this)
-
-            if(TextUtils.isEmpty(clipboardUrl)){
-                url = ""
-            }else{
-                url = clipboardUrl
-            }
-
-
-
-            if (start == end) {
-                anchor = ""
-            } else {
-                anchor = aztec.text.substring(start, end)
-            }
-        }
-
-
-        val builder = AlertDialog.Builder(this)
-        builder.setCancelable(false)
-
-        val view = layoutInflater.inflate(R.layout.dialog_link, null, false)
-        val urlInput = view.findViewById(R.id.linkURL) as EditText
-        val anchorInput = view.findViewById(R.id.linkText) as EditText
-
-        urlInput.setText(url)
-        anchorInput.setText(anchor)
-
-        builder.setView(view)
-        builder.setTitle(R.string.dialog_title)
-
-        builder.setPositiveButton(R.string.dialog_button_ok, DialogInterface.OnClickListener { dialog, which ->
-            val link = urlInput.text.toString().trim { it <= ' ' }
-            val anchorText = anchorInput.text.toString().trim { it <= ' ' }
-
-            if (TextUtils.isEmpty(link)) {
-                return@OnClickListener
-            }
-
-            if (modifyingExistingLink) {
-                aztec.editLink(link, anchorText, start, end)
-            } else {
-                aztec.addLink(link, anchorText, start, end)
-            }
-
-        })
-        builder.create().show()
-    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -225,17 +72,4 @@ class MainActivity : AppCompatActivity(), FormatToolbar.OnToolbarActionListener 
     }
 
 
-    /**
-     * Checks the Clipboard for text that matches the [Patterns.WEB_URL] pattern.
-
-     * @return the URL text in the clipboard, if it exists; otherwise null
-     */
-    fun getUrlFromClipboard(context: Context?): String? {
-        if (context == null) return null
-        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val data = clipboard?.primaryClip
-        if (data == null || data.itemCount <= 0) return null
-        val clipText = data.getItemAt(0).text.toString()
-        return if (Patterns.WEB_URL.matcher(clipText).matches()) clipText else null
-    }
 }
--- aztec/src/main/kotlin/org/wordpress/aztec/toolbar/FormatToolbar.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/toolbar/FormatToolbar.kt
@@ -1,17 +1,28 @@
 package org.wordpress.aztec.toolbar
 
+import android.app.AlertDialog
+import android.content.ClipboardManager
 import android.content.Context
+import android.content.DialogInterface
+import android.text.TextUtils
+import android.text.style.URLSpan
 import android.util.AttributeSet
+import android.util.Patterns
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import android.widget.FrameLayout
+import android.widget.Toast
 import android.widget.ToggleButton
+import org.wordpress.aztec.AztecText
 import org.wordpress.aztec.R
+import org.wordpress.aztec.TextFormat
 import java.util.*
 
 
 class FormatToolbar : FrameLayout {
 
-    private var mToolbarActionListener: OnToolbarActionListener? = null
+    private var mEditor: AztecText? = null
 
     constructor(context: Context) : super(context) {
         initView()
@@ -25,6 +36,22 @@ class FormatToolbar : FrameLayout {
         initView()
     }
 
+
+    fun isEditorAttached(): Boolean {
+        return mEditor != null
+    }
+
+    fun setEditor(editor: AztecText, initialText: String) {
+        mEditor = editor
+        mEditor!!.setText(initialText)
+        //highlight toolbar buttons based on what styles are applied to the text beneath cursor
+        mEditor!!.setOnSelectionChangedListener(object : AztecText.OnSelectionChangedListener {
+            override fun onSelectionChanged(selStart: Int, selEnd: Int) {
+                highlightAppliedStyles(selStart, selEnd)
+            }
+        })
+    }
+
     private fun initView() {
         View.inflate(context, R.layout.format_bar, this)
 
@@ -32,19 +59,14 @@ class FormatToolbar : FrameLayout {
             val button = findViewById(toolbarAction.buttonId)
             button?.setOnClickListener { sendToolbarEvent(toolbarAction) }
         }
+
     }
 
     private fun sendToolbarEvent(toolbarAction: ToolbarAction) {
-        mToolbarActionListener?.onToolbarAction(toolbarAction)
+        onToolbarAction(toolbarAction)
     }
 
-    fun setToolbarActionListener(toolbarActionListener: OnToolbarActionListener) {
-        mToolbarActionListener = toolbarActionListener
-    }
 
-    interface OnToolbarActionListener {
-        fun onToolbarAction(action: ToolbarAction)
-    }
 
     fun highlightActionButtons(toolbarActions: ArrayList<ToolbarAction>) {
         ToolbarAction.values().forEach { action ->
@@ -72,6 +94,160 @@ class FormatToolbar : FrameLayout {
         if (button != null && button is ToggleButton) {
             button.isChecked = checked
         }
+    }
 
+
+    fun highlightAppliedStyles(selStart: Int, selEnd: Int) {
+        if (!isEditorAttached()) return
+
+        var newSelStart = selStart
+
+        if (selStart > 0 && !mEditor!!.isTextSelected()) {
+            newSelStart = selStart - 1
+        }
+
+        val appliedStyles = mEditor!!.getAppliedStyles(newSelStart, selEnd)
+
+        //remember the last active style after emptying EditText manually
+        if (!mEditor!!.isEmpty()) {
+            mEditor!!.setSelectedStyles(appliedStyles)
+            highlightActionButtons(ToolbarAction.getToolbarActionsForStyles(appliedStyles))
+        }
+    }
+
+
+    fun onToolbarAction(action: ToolbarAction) {
+        if (!isEditorAttached()) return
+        //if noting is selected just activate style
+        if (!mEditor!!.isTextSelected() && action.actionType == ToolbarActionType.INLINE_STYLE) {
+            val actions = getSelectedActions()
+            val textFormats = ArrayList<TextFormat>()
+
+            actions.forEach { if (it.isStylingAction()) textFormats.add(it.textFormat!!) }
+            return mEditor!!.setSelectedStyles(textFormats)
+        }
+
+        //if text is selected and action is styling toggle it's style
+        if (action.isStylingAction()) {
+            return mEditor!!.toggleFormatting(action.textFormat!!)
+        }
+
+        //other toolbar action
+        when (action) {
+            ToolbarAction.LINK -> showLinkDialog()
+            ToolbarAction.HTML -> mEditor!!.setText(mEditor!!.toHtml())
+            else -> {
+                Toast.makeText(context, "Unsupported action", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    private fun showLinkDialog() {
+        if (!isEditorAttached()) return
+
+        var start = mEditor!!.selectionStart
+        var end = mEditor!!.selectionEnd
+
+        val urlSpans = mEditor!!.text.getSpans(start, end, URLSpan::class.java)
+
+
+        val url: String?
+        var anchor: String?
+
+        var modifyingExistingLink = false
+
+        var spanStart: Int = 0
+        var spanEnd: Int = 0
+
+        if (!urlSpans.isEmpty()) {
+            val urlSpan = urlSpans[0]
+
+            spanStart = mEditor!!.text.getSpanStart(urlSpan)
+            spanEnd = mEditor!!.text.getSpanEnd(urlSpan)
+
+            if (start < spanStart || end > spanEnd) {
+                //looks like some text that is not part of the url was included in selection
+                anchor = mEditor!!.text.substring(start, end)
+                url = ""
+
+            } else {
+                anchor = mEditor!!.text.substring(spanStart, spanEnd)
+                url = urlSpan.url
+                start = spanStart
+                end = spanEnd
+            }
+
+            if (anchor.equals(url)) {
+                anchor = ""
+            }
+
+
+            modifyingExistingLink = true
+
+        } else {
+
+            val clipboardUrl = getUrlFromClipboard(context)
+
+            if (TextUtils.isEmpty(clipboardUrl)) {
+                url = ""
+            } else {
+                url = clipboardUrl
+            }
+
+
+
+            if (start == end) {
+                anchor = ""
+            } else {
+                anchor = mEditor!!.text.substring(start, end)
+            }
+        }
+
+
+        val builder = AlertDialog.Builder(context)
+        builder.setCancelable(false)
+
+        val view = LayoutInflater.from(context).inflate(R.layout.dialog_link, null, false)
+        val urlInput = view.findViewById(R.id.linkURL) as EditText
+        val anchorInput = view.findViewById(R.id.linkText) as EditText
+
+        urlInput.setText(url)
+        anchorInput.setText(anchor)
+
+        builder.setView(view)
+        builder.setTitle(R.string.dialog_title)
+
+        builder.setPositiveButton(R.string.dialog_button_ok, DialogInterface.OnClickListener { dialog, which ->
+            val link = urlInput.text.toString().trim { it <= ' ' }
+            val anchorText = anchorInput.text.toString().trim { it <= ' ' }
+
+            if (TextUtils.isEmpty(link)) {
+                return@OnClickListener
+            }
+
+            if (modifyingExistingLink) {
+                mEditor!!.editLink(link, anchorText, start, end)
+            } else {
+                mEditor!!.addLink(link, anchorText, start, end)
+            }
+
+        })
+        builder.create().show()
+    }
+
+
+    /**
+     * Checks the Clipboard for text that matches the [Patterns.WEB_URL] pattern.
+
+     * @return the URL text in the clipboard, if it exists; otherwise null
+     */
+    fun getUrlFromClipboard(context: Context?): String? {
+        if (context == null) return null
+        val clipboard = context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val data = clipboard?.primaryClip
+        if (data == null || data.itemCount <= 0) return null
+        val clipText = data.getItemAt(0).text.toString()
+        return if (Patterns.WEB_URL.matcher(clipText).matches()) clipText else null
     }
 }

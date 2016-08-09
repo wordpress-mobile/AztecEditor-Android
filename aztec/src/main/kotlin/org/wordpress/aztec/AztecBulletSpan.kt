4f85edd610996d4157fcb4d43434b6317c8918c0
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2016-08-09 09:19:34 +0900

    Changed bullet span to draw bullet on each line.

--- aztec/src/main/kotlin/org/wordpress/aztec/AztecBulletSpan.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/AztecBulletSpan.kt
@@ -22,7 +22,7 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.os.Parcel
 import android.text.Layout
-import android.text.Spanned
+import android.text.TextUtils
 import android.text.style.BulletSpan
 
 class AztecBulletSpan : BulletSpan {
@@ -30,6 +30,7 @@ class AztecBulletSpan : BulletSpan {
     private var bulletColor = DEFAULT_COLOR
     private var bulletRadius = DEFAULT_RADIUS
     private var bulletGapWidth = DEFAULT_GAP_WIDTH
+
 
     constructor(bulletColor: Int, bulletRadius: Int, bulletGapWidth: Int) {
         this.bulletColor = if (bulletColor != 0) bulletColor else DEFAULT_COLOR
@@ -58,10 +59,13 @@ class AztecBulletSpan : BulletSpan {
                                    top: Int, baseline: Int, bottom: Int,
                                    text: CharSequence, start: Int, end: Int,
                                    first: Boolean, l: Layout) {
-        if ((text as Spanned).getSpanStart(this) == start) {
+        val lines = TextUtils.split(text.toString(), "\n")
+
+        for (i in lines.indices) {
             val style = p.style
 
             val oldColor = p.color
+
             p.color = bulletColor
             p.style = Paint.Style.FILL
 

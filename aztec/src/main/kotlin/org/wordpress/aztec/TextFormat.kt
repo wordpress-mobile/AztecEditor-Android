bc68eb5ded7515e47926a2bfa17c29a11c4b69db
Tyler Heck <theck13@hotmail.com>
2016-08-31 12:03:30 -0600

    Add headers to text format

--- aztec/src/main/kotlin/org/wordpress/aztec/TextFormat.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/TextFormat.kt
@@ -4,6 +4,12 @@ package org.wordpress.aztec
  * Describes styles that could be applied to text
  */
 enum class TextFormat {
+    FORMAT_HEADER_1,
+    FORMAT_HEADER_2,
+    FORMAT_HEADER_3,
+    FORMAT_HEADER_4,
+    FORMAT_HEADER_5,
+    FORMAT_HEADER_6,
     FORMAT_BOLD,
     FORMAT_ITALIC,
     FORMAT_UNDERLINED,

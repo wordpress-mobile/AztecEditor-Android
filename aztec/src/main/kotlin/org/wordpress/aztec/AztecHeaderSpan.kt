97aaec85fe624c119a42ee859c56ec0bda95338c
Tyler Heck <theck13@hotmail.com>
2016-08-31 11:44:25 -0600

    Add tag method to header span class

--- aztec/src/main/kotlin/org/wordpress/aztec/AztecHeaderSpan.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/AztecHeaderSpan.kt
@@ -6,13 +6,22 @@ import android.text.style.LineHeightSpan
 import android.text.style.MetricAffectingSpan
 
 class AztecHeaderSpan(val mHeader: AztecHeaderSpan.Header) : MetricAffectingSpan(), LineHeightSpan {
+    companion object {
+        private val SCALE_H1: Float = 2.0f
+        private val SCALE_H2: Float = 1.8f
+        private val SCALE_H3: Float = 1.6f
+        private val SCALE_H4: Float = 1.4f
+        private val SCALE_H5: Float = 1.2f
+        private val SCALE_H6: Float = 1.0f
+    }
+
     enum class Header constructor(internal val mScale: Float) {
-        H1(2.0f),
-        H2(1.8f),
-        H3(1.6f),
-        H4(1.4f),
-        H5(1.2f),
-        H6(1.0f)
+        H1(SCALE_H1),
+        H2(SCALE_H2),
+        H3(SCALE_H3),
+        H4(SCALE_H4),
+        H5(SCALE_H5),
+        H6(SCALE_H6)
     }
 
     override fun chooseHeight(text: CharSequence, start: Int, end: Int, startspanv: Int, v: Int, fm: FontMetricsInt) {
@@ -27,5 +36,17 @@ class AztecHeaderSpan(val mHeader: AztecHeaderSpan.Header) : MetricAffectingSpan
 
     override fun updateMeasureState(textPaint: TextPaint) {
         textPaint.textSize *= mHeader.mScale
+    }
+
+    fun getTag(): String {
+        when (mHeader.mScale) {
+            SCALE_H1 -> return "h1"
+            SCALE_H2 -> return "h2"
+            SCALE_H3 -> return "h3"
+            SCALE_H4 -> return "h4"
+            SCALE_H5 -> return "h5"
+            SCALE_H6 -> return "h6"
+            else -> return "p"
+        }
     }
 }

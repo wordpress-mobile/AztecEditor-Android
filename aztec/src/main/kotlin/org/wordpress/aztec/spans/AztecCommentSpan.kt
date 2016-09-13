0992b2f0b1ef8a37bc9a3e72416c299c7b17455b
Tyler Heck <theck13@hotmail.com>
2016-09-13 10:23:00 -0600

    Add more and page types to custom comment span

--- aztec/src/main/kotlin/org/wordpress/aztec/spans/AztecCommentSpan.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/spans/AztecCommentSpan.kt
@@ -2,18 +2,21 @@ package org.wordpress.aztec.spans
 
 import android.content.Context
 import android.text.style.ImageSpan
-import android.text.style.ParagraphStyle
 import android.view.View
 import android.widget.Toast
 
-internal class AztecCommentSpan(html: StringBuilder, context: Context, drawable: Int) : ImageSpan(context, drawable), ParagraphStyle {
-    val mHtml: StringBuilder = html
+internal class AztecCommentSpan(val mComment: AztecCommentSpan.Comment, context: Context, drawable: Int) : ImageSpan(context, drawable) {
+    companion object {
+        private val HTML_MORE: String = "<!--more-->"
+        private val HTML_PAGE: String = "<!--nextpage-->"
+    }
 
-    fun getHtml(): StringBuilder {
-        return mHtml
+    enum class Comment constructor(internal val mHtml: String) {
+        MORE(HTML_MORE),
+        PAGE(HTML_PAGE)
     }
 
     fun onClick(view: View) {
-        Toast.makeText(view.context, mHtml.toString(), Toast.LENGTH_SHORT).show()
+        Toast.makeText(view.context, mComment.mHtml.toString(), Toast.LENGTH_SHORT).show()
     }
 }

354b6c01234715a9350a00f379cc6fea9a3ca18a
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2016-11-03 12:09:26 +0900

    Changed the base type of character style span

--- aztec/src/main/kotlin/org/wordpress/aztec/spans/AztecCursorSpan.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/spans/AztecCursorSpan.kt
@@ -1,12 +1,8 @@
 package org.wordpress.aztec.spans
 
-import android.text.TextPaint
-import android.text.style.CharacterStyle
 
+class AztecCursorSpan  {
 
-class AztecCursorSpan : CharacterStyle() {
-    override fun updateDrawState(tp: TextPaint?) {
-    }
 
     val TAG = "aztec_cursor"
 }

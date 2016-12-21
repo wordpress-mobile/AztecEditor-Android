179c68a0d41d33f90abe0f5cd8f42ff3bc71ab74
Ondrej Ruttkay <ondrej@automattic.com>
2016-12-21 19:55:44 +0100

    Add block element test

--- aztec/src/test/kotlin/org/wordpress/aztec/BlockElementsTest.kt
+++ aztec/src/test/kotlin/org/wordpress/aztec/BlockElementsTest.kt
@@ -76,4 +76,15 @@ class BlockElementsTest {
         Assert.assertEquals("some text<br><blockquote>quote</blockquote><br><ul><li>list</li></ul><br><br>some text", editText.toHtml())
     }
 
+    @Test
+    @Throws(Exception::class)
+    fun checkForDanglingListWithoutItems() {
+        editText.toggleFormatting(TextFormat.FORMAT_ORDERED_LIST)
+        Assert.assertEquals("<ol><li></li></ol>", editText.toHtml())
+        Assert.assertEquals(Constants.ZWJ_STRING, editText.text.toString())
+
+        editText.text.delete(0, 1)
+        Assert.assertEquals("", editText.text.toString())
+        Assert.assertEquals("", editText.toHtml())
+    }
 }

40397355adf3a360133e5777fc405bc4d266e86f
theck13 <theck13@hotmail.com>
2017-02-02 10:30:59 -0700

    Revert 'Remove br from block elements'

--- aztec/src/main/kotlin/org/wordpress/aztec/source/Format.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/source/Format.kt
@@ -6,7 +6,7 @@ import java.util.regex.Pattern
 object Format {
 
     // list of block elements
-    private val block = "div|span|blockquote|ul|ol|li|p|h1|h2|h3|h4|h5|h6"
+    private val block = "div|span|br|blockquote|ul|ol|li|p|h1|h2|h3|h4|h5|h6"
 
     fun addFormatting(content: String): String {
         val doc = Jsoup.parseBodyFragment(content)

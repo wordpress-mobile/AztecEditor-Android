9a37598c0998b463bc5133e904d19d9f80d3816f
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-02-06 17:46:37 +0200

    Fix from merge: use AztecText's insertMedia

--- app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
+++ app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
@@ -137,7 +137,7 @@ class MainActivity : AppCompatActivity(), OnMediaOptionSelectedListener, OnReque
 
             val attrs = AttributesImpl()
             attrs.addAttribute("", "src", "src", "string", mediaPath) // Temporary source value.  Replace with URL after uploaded.
-            aztec.lineBlockFormatter.insertMedia(BitmapDrawable(resources, bitmap), attrs, this)
+            aztec.insertMedia(BitmapDrawable(resources, bitmap), attrs)
         }
 
         super.onActivityResult(requestCode, resultCode, data)

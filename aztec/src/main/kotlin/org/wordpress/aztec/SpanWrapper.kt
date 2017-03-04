43fd29d331794b5dc396f45ae88ce36b0bc69728
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2017-03-04 20:14:18 +0200

    Simplify SpanWrapper

--- aztec/src/main/kotlin/org/wordpress/aztec/SpanWrapper.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/SpanWrapper.kt
@@ -2,30 +2,22 @@ package org.wordpress.aztec
 
 import android.text.Spannable
 
-import java.lang.ref.WeakReference
-
-class SpanWrapper<out T>(spannable: Spannable?, span: T) {
-    private val mSpannable: WeakReference<Spannable?> = WeakReference(spannable)
-    private val mSpan: WeakReference<T> = WeakReference(span)
-
-    val span: T
-        get() = mSpan.get()
-
+class SpanWrapper<T>(var spannable: Spannable, var span: T) {
     fun remove() {
-        mSpannable.get()?.removeSpan(span)
+        spannable.removeSpan(span)
     }
 
     var start: Int
-        get() { return mSpannable.get()?.getSpanStart(mSpan.get()) ?: -1 }
-        set(start) { mSpannable.get()?.setSpan(mSpan.get(), start, end, flags) }
+        get() { return spannable.getSpanStart(span) }
+        set(start) { spannable.setSpan(span, start, end, flags) }
 
     var end: Int
-        get() { return mSpannable.get()?.getSpanEnd(mSpan.get()) ?: -1 }
-        set(end) { mSpannable.get()?.setSpan(mSpan.get(), start, end, flags) }
+        get() { return spannable.getSpanEnd(span) }
+        set(end) { spannable.setSpan(span, start, end, flags) }
 
     var flags: Int
-        get() { return mSpannable.get()?.getSpanFlags(mSpan.get()) ?: -1 }
-        set(flags) { mSpannable.get()?.setSpan(mSpan.get(), start, end, flags) }
+        get() { return spannable.getSpanFlags(span) }
+        set(flags) { spannable.setSpan(span, start, end, flags) }
 
     companion object {
         fun <T> getSpans(spannable: Spannable, start: Int, end: Int, type: Class<T>): List<SpanWrapper<T>> {

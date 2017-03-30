c6a7ff45cd7f7b636673de97a25eeee78ffbd8d3
Ondrej Ruttkay <ondrej@automattic.com>
2017-03-31 00:42:55 +0200

    Fix comment drawable bounds when adding it from toolbar

--- aztec/src/main/kotlin/org/wordpress/aztec/spans/AztecCommentSpan.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/spans/AztecCommentSpan.kt
@@ -2,9 +2,15 @@ package org.wordpress.aztec.spans
 
 import android.content.Context
 import android.graphics.drawable.Drawable
+import org.wordpress.aztec.AztecText
 
-class AztecCommentSpan(context: Context, drawable: Drawable, override var nestingLevel: Int) :
+class AztecCommentSpan @JvmOverloads constructor(context: Context, drawable: Drawable, override var nestingLevel: Int, editor: AztecText? = null) :
         AztecDynamicImageSpan(context, drawable), AztecFullWidthImageSpan {
+
+    init {
+        textView = editor
+    }
+
     companion object {
         private val HTML_MORE: String = "more"
         private val HTML_PAGE: String = "nextpage"

5a37fd7f5555fd6d899284e7bbe60a740f7f6c0a
Ondrej Ruttkay <ondrej@automattic.com>
2017-04-13 18:20:52 +0200

    Add attribute class for easier element manipulation

---
+++ aztec/src/main/kotlin/org/wordpress/aztec/AztecAttributes.kt
@@ -0,0 +1,35 @@
+package org.wordpress.aztec
+
+import org.xml.sax.Attributes
+import org.xml.sax.helpers.AttributesImpl
+
+class AztecAttributes(attributes: Attributes = AttributesImpl()) : AttributesImpl(attributes) {
+
+    fun setValue(key: String, value: String) {
+        val index = getIndex(key)
+        if (index == -1) {
+            addAttribute("", key, key, "string", value)
+        } else {
+            setValue(index, value)
+        }
+    }
+
+    fun isEmpty() : Boolean {
+        return length == 0
+    }
+
+    fun hasAttribute(key: String) : Boolean {
+        return getValue(key) == null
+    }
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        for (i in 0..this.length-1) {
+            sb.append(this.getLocalName(i))
+            sb.append("=\"")
+            sb.append(this.getValue(i))
+            sb.append("\" ")
+        }
+        return sb.trimEnd().toString()
+    }
+}

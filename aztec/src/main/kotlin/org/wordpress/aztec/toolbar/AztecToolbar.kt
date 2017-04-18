b652be218712bca2ba6d45ee81049614d979bd51
theck13 <theck13@hotmail.com>
2017-04-18 15:47:35 -0500

    Add logic to heading menu selection for unformatted text

--- aztec/src/main/kotlin/org/wordpress/aztec/toolbar/AztecToolbar.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/toolbar/AztecToolbar.kt
@@ -360,21 +360,26 @@ class AztecToolbar : FrameLayout, OnMenuItemClickListener {
     }
 
     private fun selectHeadingMenuItem(textFormats: ArrayList<TextFormat>) {
-        textFormats.forEach {
-            when (it) {
-                TextFormat.FORMAT_HEADING_1 -> headingMenu?.menu?.getItem(1)?.isChecked = true
-                TextFormat.FORMAT_HEADING_2 -> headingMenu?.menu?.getItem(2)?.isChecked = true
-                TextFormat.FORMAT_HEADING_3 -> headingMenu?.menu?.getItem(3)?.isChecked = true
-                TextFormat.FORMAT_HEADING_4 -> headingMenu?.menu?.getItem(4)?.isChecked = true
-                TextFormat.FORMAT_HEADING_5 -> headingMenu?.menu?.getItem(5)?.isChecked = true
-                TextFormat.FORMAT_HEADING_6 -> headingMenu?.menu?.getItem(6)?.isChecked = true
-                else -> {
-                    // Select TextFormat.FORMAT_PARAGRAPH by default.
-                    headingMenu?.menu?.getItem(0)?.isChecked = true
+        if (textFormats.size == 0) {
+            // Select TextFormat.FORMAT_PARAGRAPH by default.
+            headingMenu?.menu?.getItem(0)?.isChecked = true
+        } else {
+            textFormats.forEach {
+                when (it) {
+                    TextFormat.FORMAT_HEADING_1 -> headingMenu?.menu?.getItem(1)?.isChecked = true
+                    TextFormat.FORMAT_HEADING_2 -> headingMenu?.menu?.getItem(2)?.isChecked = true
+                    TextFormat.FORMAT_HEADING_3 -> headingMenu?.menu?.getItem(3)?.isChecked = true
+                    TextFormat.FORMAT_HEADING_4 -> headingMenu?.menu?.getItem(4)?.isChecked = true
+                    TextFormat.FORMAT_HEADING_5 -> headingMenu?.menu?.getItem(5)?.isChecked = true
+                    TextFormat.FORMAT_HEADING_6 -> headingMenu?.menu?.getItem(6)?.isChecked = true
+                    else -> {
+                        // Select TextFormat.FORMAT_PARAGRAPH by default.
+                        headingMenu?.menu?.getItem(0)?.isChecked = true
+                    }
                 }
-            }
 
-            return
+                return
+            }
         }
     }
 

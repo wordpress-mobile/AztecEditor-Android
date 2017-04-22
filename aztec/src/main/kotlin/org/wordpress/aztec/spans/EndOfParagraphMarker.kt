4be737a8b4532c6ae9bb13b855a3c92638c843f7
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2017-04-22 11:15:09 +0900

    Fixed miscalculated height paragraph span.

--- aztec/src/main/kotlin/org/wordpress/aztec/spans/EndOfParagraphMarker.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/spans/EndOfParagraphMarker.kt
@@ -1,6 +1,7 @@
 package org.wordpress.aztec.spans
 
 import android.graphics.Paint
+import android.text.Spanned
 import android.text.style.LineHeightSpan
 import android.text.style.UpdateLayout
 
@@ -8,7 +9,11 @@ import android.text.style.UpdateLayout
 class EndOfParagraphMarker(var verticalPadding: Int = 0) : LineHeightSpan, UpdateLayout {
 
     override fun chooseHeight(text: CharSequence?, start: Int, end: Int, spanstartv: Int, v: Int, fm: Paint.FontMetricsInt) {
-        fm.descent += verticalPadding
-        fm.bottom += verticalPadding
+        val spanned = text as Spanned
+        val spanEnd = spanned.getSpanEnd(this)
+        if (end == spanEnd) {
+            fm.descent += verticalPadding
+            fm.bottom += verticalPadding
+        }
     }
 }

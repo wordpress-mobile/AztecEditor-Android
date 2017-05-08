e4a6d2b7c6a225adefc30c168dd4a8600be4000a
Ondrej Ruttkay <ondrej@automattic.com>
2017-05-09 00:00:50 +0200

    Add comment handler plugin implementation

---
+++ wordpress-comments/src/main/java/WordPressCommentsPlugin.kt
@@ -0,0 +1,49 @@
+import android.content.Context
+import android.text.Editable
+import android.text.Spannable
+import org.wordpress.aztec.Constants
+import org.wordpress.aztec.R
+import org.wordpress.aztec.plugins.IAztecPlugin
+import org.wordpress.aztec.plugins.html2visual.IAztecCommentHandler
+import org.wordpress.aztec.plugins.wpcomments.spans.WordPressCommentSpan
+import org.wordpress.aztec.spans.AztecCommentSpan
+
+class WordPressCommentsPlugin : IAztecPlugin, IAztecCommentHandler {
+
+    override fun handleComment(text: String, output: Editable, context: Context, nestingLevel: Int) {
+
+        val spanStart = output.length
+
+        if (text.toLowerCase() == AztecCommentSpan.Comment.MORE.html.toLowerCase()) {
+
+            output.append(Constants.MAGIC_CHAR)
+
+            output.setSpan(
+                    WordPressCommentSpan(
+                            text,
+                            context,
+                            context.resources.getDrawable(R.drawable.img_more),
+                            nestingLevel
+                    ),
+                    spanStart,
+                    text.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        } else if (text.toLowerCase() == AztecCommentSpan.Comment.PAGE.html.toLowerCase()) {
+
+            output.append(Constants.MAGIC_CHAR)
+
+            output.setSpan(
+                    WordPressCommentSpan(
+                            text,
+                            context,
+                            context.resources.getDrawable(R.drawable.img_page),
+                            nestingLevel
+                    ),
+                    spanStart,
+                    text.length,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+    }
+}

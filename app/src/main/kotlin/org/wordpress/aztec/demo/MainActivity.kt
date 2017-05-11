e239bb92a18188d38cdbb8ab1c51c8b1a91eafda
rachelmcr <rachel@automattic.com>
2017-05-11 22:49:26 +0100

    Setup for Espresso tests

--- app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
+++ app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
@@ -119,6 +119,8 @@ class MainActivity : AppCompatActivity(),
                 UNKNOWN +
                 EMOJI +
                 LONG_TEXT
+
+        var testMode: Boolean = false
     }
 
     private val MEDIA_CAMERA_PHOTO_PERMISSION_REQUEST_CODE: Int = 1001
@@ -256,7 +258,9 @@ class MainActivity : AppCompatActivity(),
         aztec.setToolbar(formattingToolbar)
 
         // initialize the text & HTML
-        source.displayStyledAndFormattedHtml(EXAMPLE)
+        if (!testMode) {
+            source.displayStyledAndFormattedHtml(EXAMPLE)
+        }
 
         if (savedInstanceState == null) {
             aztec.fromHtml(source.getPureHtml())

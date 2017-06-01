eedebbbcafe879e805ee0183bc0096b896da3425
Ondrej Ruttkay <ondrej@automattic.com>
2017-06-01 18:17:48 +0200

    Handle video HTML tag

--- aztec/src/main/kotlin/org/wordpress/aztec/AztecTagHandler.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/AztecTagHandler.kt
@@ -78,6 +78,18 @@ class AztecTagHandler : Html.TagHandler {
                 }
                 return true
             }
+            VIDEO -> {
+                if (opening) {
+                    val mediaSpan = createVideoSpan(AztecAttributes(attributes), onMediaTappedListener, context, nestingLevel)
+                    start(output, mediaSpan)
+                    start(output, AztecMediaClickableSpan(mediaSpan))
+                    output.append(Constants.IMG_CHAR)
+                } else {
+                    end(output, AztecVideoSpan::class.java)
+                    end(output, AztecMediaClickableSpan::class.java)
+                }
+                return true
+            }
             PARAGRAPH -> {
                 handleElement(output, opening, ParagraphSpan(nestingLevel, AztecAttributes(attributes)))
                 return true
@@ -113,6 +125,14 @@ class AztecTagHandler : Html.TagHandler {
         val loadingDrawable = ContextCompat.getDrawable(context, styles.getResourceId(R.styleable.AztecText_drawableLoading, R.drawable.ic_image_loading))
         styles.recycle()
         return AztecImageSpan(context, loadingDrawable, attributes, onMediaTappedListener)
+    }
+
+    private fun createVideoSpan(attributes: AztecAttributes, onMediaTappedListener: AztecText.OnMediaTappedListener?,
+                                context: Context, nestingLevel: Int) : AztecMediaSpan {
+        val styles = context.obtainStyledAttributes(R.styleable.AztecText)
+        val loadingDrawable = ContextCompat.getDrawable(context, styles.getResourceId(R.styleable.AztecText_drawableLoading, R.drawable.ic_image_loading))
+        styles.recycle()
+        return AztecVideoSpan(context, loadingDrawable, nestingLevel, attributes, onMediaTappedListener)
     }
 
     private fun handleElement(output: Editable, opening: Boolean, span: Any) {
@@ -170,6 +190,7 @@ class AztecTagHandler : Html.TagHandler {
         private val PARAGRAPH = "p"
         private val PREFORMAT = "pre"
         private val IMAGE = "img"
+        private val VIDEO = "video"
         private val LINE = "hr"
 
         private fun getLast(text: Editable, kind: Class<*>): Any? {

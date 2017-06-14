9c704b85ceef0ef4ff2349111604db8c5ecd2647
theck13 <theck13@hotmail.com>
2017-06-13 18:51:01 -0600

    Add horizontal rule format button functionality

--- aztec/src/main/kotlin/org/wordpress/aztec/formatting/LineBlockFormatter.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/formatting/LineBlockFormatter.kt
@@ -118,6 +118,31 @@ class LineBlockFormatter(editor: AztecText) : AztecFormatter(editor) {
                 if (selectionEnd < EndOfBufferMarkerAdder.safeLength(editor)) selectionEnd + 1 else selectionEnd)
     }
 
+    fun applyHorizontalRule() {
+        editor.removeInlineStylesFromRange(selectionStart, selectionEnd)
+        editor.removeBlockStylesFromRange(selectionStart, selectionEnd, true)
+
+        val nestingLevel = AztecNestable.getNestingLevelAt(editableText, selectionStart)
+
+        val span = AztecHorizontalLineSpan(
+            editor.context,
+            ContextCompat.getDrawable(editor.context, R.drawable.img_hr),
+            nestingLevel
+        )
+
+        val builder = SpannableStringBuilder(Constants.MAGIC_STRING)
+        builder.setSpan(span, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        editableText.replace(selectionStart, selectionEnd, builder)
+
+        editor.setSelection(
+            if (selectionEnd < EndOfBufferMarkerAdder.safeLength(editor)) {
+                selectionEnd + 1
+            } else {
+                selectionEnd
+            }
+        )
+    }
 
     fun insertVideo(drawable: Drawable?, attributes: Attributes, onVideoTappedListener: OnVideoTappedListener?): AztecMediaSpan {
         val nestingLevel = AztecNestable.getNestingLevelAt(editableText, selectionStart)

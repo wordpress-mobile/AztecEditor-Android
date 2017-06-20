eec5901665889fce60cc080a92e5ebca52dfaeb4
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2017-06-20 19:02:42 +0900

    Added check for EOBM when deleting stuff

--- aztec/src/main/kotlin/org/wordpress/aztec/watchers/TextChangedEvent.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/watchers/TextChangedEvent.kt
@@ -67,6 +67,9 @@ data class TextChangedEvent(val textBefore: CharSequence = "", val deletedFromBl
         if (isAddingCharacters && numberOfAddedCharacters == 1) {
             val currentCharacter = text[inputStart]
             return currentCharacter == Constants.END_OF_BUFFER_MARKER
+        }else if(!isAddingCharacters && numberOfRemovedCharacters == 1){
+            val removedCharacter = textBefore[inputEnd]
+            return removedCharacter == Constants.END_OF_BUFFER_MARKER
         }
         return false
     }

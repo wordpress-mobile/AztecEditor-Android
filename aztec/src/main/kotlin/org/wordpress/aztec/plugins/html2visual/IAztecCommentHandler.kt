07f734ea842de12d3f058fb1b66c8b81915968a6
Ondrej Ruttkay <ondrej@automattic.com>
2017-07-06 21:29:38 +0200

    Update comment handler plugin interface

--- aztec/src/main/kotlin/org/wordpress/aztec/plugins/html2visual/IAztecCommentHandler.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/plugins/html2visual/IAztecCommentHandler.kt
@@ -1,9 +1,27 @@
 package org.wordpress.aztec.plugins.html2visual
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.text.Editable
+import android.text.Spanned
+import android.text.style.CharacterStyle
 import org.wordpress.aztec.plugins.IAztecPlugin
 
+@SuppressLint("NewApi")
 interface IAztecCommentHandler : IAztecPlugin {
-    fun handleComment(text: String, output: Editable, context: Context, nestingLevel: Int) : Boolean
+
+    fun canHandle(span: CharacterStyle): Boolean {
+        return true
+    }
+
+    fun shouldParseContent(): Boolean {
+        return true
+    }
+
+    fun handleCommentHtml(text: String, output: Editable, context: Context, nestingLevel: Int) : Boolean {
+        return true
+    }
+
+    fun handleCommentSpanStart(out: StringBuilder, span: CharacterStyle)
+    fun handleCommentSpanEnd(out: StringBuilder, span: CharacterStyle)
 }

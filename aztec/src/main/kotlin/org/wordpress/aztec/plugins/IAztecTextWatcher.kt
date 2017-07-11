4f04942b432f772a92b91e9b680f6b1d27f1f164
Ondrej Ruttkay <ondrej@automattic.com>
2017-07-11 02:17:12 +0200

    Remove unrelated plugin interfaces

--- aztec/src/main/kotlin/org/wordpress/aztec/plugins/IAztecTextWatcher.kt
+++
@@ -1,3 +0,0 @@
-package org.wordpress.aztec.plugins
-
-interface IAztecTextWatcher : IAztecPlugin
--- aztec/src/main/kotlin/org/wordpress/aztec/plugins/html2visual/IAztecHtmlToSpanPostprocessor.kt
+++
@@ -1,5 +0,0 @@
-package org.wordpress.aztec.plugins.html2visual
-
-import org.wordpress.aztec.plugins.IAztecPlugin
-
-interface IAztecHtmlToSpanPostprocessor : IAztecPlugin

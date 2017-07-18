e3d12ec36804ec6769c6a2ef63afaf224e8c334a
Ondrej Ruttkay <ondrej@automattic.com>
2017-07-18 12:47:14 +0200

    Add new HTML text watcher plugin interface

---
+++ aztec/src/main/kotlin/org/wordpress/aztec/plugins/html2visual/IHtmlTextHandler.kt
@@ -0,0 +1,10 @@
+package org.wordpress.aztec.plugins.html2visual
+
+import android.text.Editable
+import org.wordpress.aztec.plugins.IAztecPlugin
+
+interface IHtmlTextHandler : IAztecPlugin {
+    val pattern: String
+
+    fun onHtmlTextMatch(text: String, output: Editable, nestingLevel: Int) : Boolean
+}

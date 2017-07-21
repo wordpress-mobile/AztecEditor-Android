0098fe919dc797c7dcf742b20f102d0ce854f2ce
Ondrej Ruttkay <ondrej@automattic.com>
2017-07-21 19:26:05 +0200

    Make caption span a CharacterStyle

--- wordpress-shortcodes/src/main/java/org/wordpress/aztec/plugins/shortcodes/spans/CaptionShortcodeSpan.kt
+++ wordpress-shortcodes/src/main/java/org/wordpress/aztec/plugins/shortcodes/spans/CaptionShortcodeSpan.kt
@@ -1,5 +1,11 @@
 package org.wordpress.aztec.plugins.shortcodes.spans
 
-class CaptionShortcodeSpan(val attrs: Map<String, String>) {
+import android.text.TextPaint
+import android.text.style.CharacterStyle
+
+class CaptionShortcodeSpan(val attrs: Map<String, String>) : CharacterStyle() {
     var caption: String = ""
+
+    override fun updateDrawState(tp: TextPaint?) {
+    }
 }

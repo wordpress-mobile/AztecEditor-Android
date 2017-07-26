342f15a05408ca3b7ecbd0c955c415c00657f636
Ondrej Ruttkay <ondrej@automattic.com>
2017-07-26 21:56:55 +0200

    Add an inline block span type

---
+++ aztec/src/main/kotlin/org/wordpress/aztec/spans/IAztecInlineBlockSpan.kt
@@ -0,0 +1,6 @@
+package org.wordpress.aztec.spans
+
+/**
+ * A block span that's not a paragraph (it is not surrounded by newlines)
+ */
+interface IAztecInlineBlockSpan : IAztecSpan, IAztecNestable

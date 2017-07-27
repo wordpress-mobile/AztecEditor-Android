0edbe781a59ef70c14aa71f15a33ef9e161f8731
Ondrej Ruttkay <ondrej@automattic.com>
2017-07-27 11:58:46 +0200

    Make the caption span a block span and styled as italic

--- wordpress-shortcodes/src/main/java/org/wordpress/aztec/plugins/shortcodes/spans/CaptionShortcodeSpan.kt
+++ wordpress-shortcodes/src/main/java/org/wordpress/aztec/plugins/shortcodes/spans/CaptionShortcodeSpan.kt
@@ -1,12 +1,14 @@
 package org.wordpress.aztec.plugins.shortcodes.spans
 
-import android.text.TextPaint
-import android.text.style.CharacterStyle
-import android.text.style.ParagraphStyle
+import android.graphics.Typeface
+import android.text.style.StyleSpan
 import org.wordpress.aztec.AztecAttributes
 import org.wordpress.aztec.spans.*
-import org.xml.sax.Attributes
 
 class CaptionShortcodeSpan(override var attributes: AztecAttributes,
                            override val TAG: String, override var nestingLevel: Int)
-    : IAztecInlineBlockSpan
+    : StyleSpan(Typeface.ITALIC), IAztecBlockSpan {
+
+    override var endBeforeBleed: Int = -1
+    override var startBeforeCollapse: Int = -1
+}

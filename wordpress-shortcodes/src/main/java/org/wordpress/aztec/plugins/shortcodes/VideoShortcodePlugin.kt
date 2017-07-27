5b08c583dd73cef7e3aafd808df3634e0368d664
Ondrej Ruttkay <ondrej@automattic.com>
2017-07-27 15:36:27 +0200

    Add new video shortcode plugin

---
+++ wordpress-shortcodes/src/main/java/org/wordpress/aztec/plugins/shortcodes/VideoShortcodePlugin.kt
@@ -0,0 +1,17 @@
+package org.wordpress.aztec.plugins.shortcodes
+
+import org.wordpress.aztec.plugins.html2visual.IHtmlPreprocessor
+import org.wordpress.aztec.plugins.visual2html.IHtmlPostprocessor
+
+class VideoShortcodePlugin : IHtmlPreprocessor, IHtmlPostprocessor {
+
+    private val TAG = "video"
+
+    override fun processHtmlBeforeParsing(source: String): String {
+        return source.replace(Regex("\\[$TAG([^\\]]*)\\]"), "<$TAG$1 />")
+    }
+
+    override fun processHtmlAfterSerialization(source: String): String {
+        return source.replace(Regex("<$TAG([^>]*)/?>"), "[$TAG$1]")
+    }
+}

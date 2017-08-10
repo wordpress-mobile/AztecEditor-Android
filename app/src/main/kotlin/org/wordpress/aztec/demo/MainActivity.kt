de149998a55f9b6e68fc5a2f89d4bec2c5dfc0c4
Ondrej Ruttkay <ondrej@automattic.com>
2017-08-10 21:50:01 +0200

    Enable Calypso mode

--- app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
+++ app/src/main/kotlin/org/wordpress/aztec/demo/MainActivity.kt
@@ -316,8 +316,6 @@ class MainActivity : AppCompatActivity(),
         val sourceEditor = findViewById(R.id.source) as SourceViewEditText
         val toolbar = findViewById(R.id.formatting_toolbar) as AztecToolbar
 
-        visualEditor.setCalypsoMode(false)
-        sourceEditor.setCalypsoMode(false)
 
         aztec = Aztec.with(visualEditor, sourceEditor, toolbar, this)
             .setImageGetter(PicassoImageLoader(this, visualEditor))

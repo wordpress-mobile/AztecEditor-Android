ed9a86c414270c37f666fecf4e676a8eb32f97a8
Ondrej Ruttkay <ondrej@automattic.com>
2017-08-25 18:58:27 +0200

    Initialize the toolbar after initializing the source editor

--- aztec/src/main/kotlin/org/wordpress/aztec/Aztec.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/Aztec.kt
@@ -26,7 +26,6 @@ open class Aztec private constructor(val visualEditor: AztecText, val toolbar: A
     var sourceEditor: SourceViewEditText? = null
 
     init {
-        initHistory()
         initToolbar()
     }
 
@@ -43,6 +42,9 @@ open class Aztec private constructor(val visualEditor: AztecText, val toolbar: A
     private constructor(visualEditor: AztecText, sourceEditor: SourceViewEditText,
                 toolbar: AztecToolbar, toolbarClickListener: IAztecToolbarClickListener) : this(visualEditor, toolbar, toolbarClickListener) {
         this.sourceEditor = sourceEditor
+
+        initToolbar()
+        initHistory()
     }
 
     companion object Factory {

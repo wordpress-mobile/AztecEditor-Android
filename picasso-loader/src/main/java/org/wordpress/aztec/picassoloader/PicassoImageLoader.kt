2c12f0f9e06d774fc7a5691c95f5cb4ee271d59b
Danilo Ercoli <ercoli@gmail.com>
2017-09-21 10:17:02 +0200

    Back to the previous Picasso loader implementation, since optimizations were not working fine

--- picasso-loader/src/main/java/org/wordpress/aztec/picassoloader/PicassoImageLoader.kt
+++ picasso-loader/src/main/java/org/wordpress/aztec/picassoloader/PicassoImageLoader.kt
@@ -5,14 +5,11 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.support.v4.util.ArrayMap
-import android.support.v4.util.LruCache
 import android.util.DisplayMetrics
 import com.squareup.picasso.MemoryPolicy
-
 import com.squareup.picasso.Picasso
 import com.squareup.picasso.Target
 import com.squareup.picasso.Transformation
-
 import org.wordpress.aztec.AztecText
 import org.wordpress.aztec.Html
 
@@ -20,34 +17,14 @@ class PicassoImageLoader(private val context: Context, aztec: AztecText) : Html.
     private val targets: MutableMap<String, com.squareup.picasso.Target>
     private val transformations: ArrayList<Transformation> = ArrayList()
 
-    companion object {
-        // Images cache shared in all instances of editors
-        val cacheSize = 4 * 1024 * 1024 // 4MiB
-        @JvmStatic val bitmapCache = object : LruCache<String, Bitmap>(cacheSize) {
-            override fun sizeOf(key: String?, value: Bitmap?): Int {
-                return value?.byteCount ?: 1
-            }
-        }
-    }
-
     init {
         this.targets = ArrayMap<String, Target>()
 
         // Picasso keeps a weak reference to targets so we need to attach them to AztecText
         aztec.tag = targets
-        transformations.add(CompressTransformation())
     }
 
     override fun loadImage(source: String, callbacks: Html.ImageGetter.Callbacks, maxWidth: Int) {
-        val cacheKey = source + maxWidth
-        synchronized (bitmapCache) {
-            if (bitmapCache.get(cacheKey) != null) {
-                callbacks.onImageLoaded(BitmapDrawable(context.resources, bitmapCache.get(cacheKey)))
-                return
-            }
-        }
-
-
         val picasso = Picasso.with(context)
         picasso.isLoggingEnabled = true
 
@@ -56,9 +33,6 @@ class PicassoImageLoader(private val context: Context, aztec: AztecText) : Html.
                 bitmap?.density = DisplayMetrics.DENSITY_DEFAULT
                 callbacks.onImageLoaded(BitmapDrawable(context.resources, bitmap))
                 targets.remove(source)
-                synchronized (bitmapCache) {
-                    bitmapCache.put(cacheKey, bitmap)
-                }
             }
 
             override fun onBitmapFailed(errorDrawable: Drawable?) {

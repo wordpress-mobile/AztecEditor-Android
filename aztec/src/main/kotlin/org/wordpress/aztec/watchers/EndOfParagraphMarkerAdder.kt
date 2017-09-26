310e65f931d8c1f8d3696d1031771d262081d4a1
Ondrej Ruttkay <ondrej@automattic.com>
2017-09-26 12:44:06 +0200

    Split a paragraph into 2 when adding a newline inside in Calypso mode

--- aztec/src/main/kotlin/org/wordpress/aztec/watchers/EndOfParagraphMarkerAdder.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/watchers/EndOfParagraphMarkerAdder.kt
@@ -33,6 +33,18 @@ class EndOfParagraphMarkerAdder(aztecText: AztecText, val verticalParagraphMargi
             if (paragraphMarkerCanBeApplied(aztecText.text)) {
                 aztecText.text.setSpan(EndOfParagraphMarker(verticalParagraphMargin), inputStart, inputEnd,
                         Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                // if we added a newline inside a paragraph, we should split it up into 2
+                val paragraphs = aztecText.text.getSpans(inputStart, inputEnd, ParagraphSpan::class.java)
+                if (paragraphs.isNotEmpty()) {
+                    val paragraph = paragraphs.first()
+                    val paragraphEnd = aztecText.text.getSpanEnd(paragraph)
+                    if (paragraphEnd > inputEnd) {
+                        val paragraphStart = aztecText.text.getSpanStart(paragraph)
+                        val paragraphFlags = aztecText.text.getSpanFlags(paragraph)
+                        aztecText.text.setSpan(paragraph, paragraphStart, inputEnd, paragraphFlags)
+                    }
+                }
             }
         }
     }

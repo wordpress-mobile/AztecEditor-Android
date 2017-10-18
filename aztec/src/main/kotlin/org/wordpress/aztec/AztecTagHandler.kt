8977fac1d024d4a61e363e50f89369092dc90565
Danilo Ercoli <ercoli@gmail.com>
2017-10-18 12:19:53 +0200

    Format AztecTagHadler class

--- aztec/src/main/kotlin/org/wordpress/aztec/AztecTagHandler.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/AztecTagHandler.kt
@@ -29,10 +29,25 @@ import android.text.Spannable
 import android.text.Spanned
 import org.wordpress.aztec.plugins.IAztecPlugin
 import org.wordpress.aztec.plugins.html2visual.IHtmlTagHandler
-import org.wordpress.aztec.spans.*
+import org.wordpress.aztec.spans.AztecAudioSpan
+import org.wordpress.aztec.spans.AztecHeadingSpan
+import org.wordpress.aztec.spans.AztecHorizontalRuleSpan
+import org.wordpress.aztec.spans.AztecImageSpan
+import org.wordpress.aztec.spans.AztecListItemSpan
+import org.wordpress.aztec.spans.AztecMediaClickableSpan
+import org.wordpress.aztec.spans.AztecMediaSpan
+import org.wordpress.aztec.spans.AztecOrderedListSpan
+import org.wordpress.aztec.spans.AztecPreformatSpan
+import org.wordpress.aztec.spans.AztecQuoteSpan
+import org.wordpress.aztec.spans.AztecStrikethroughSpan
+import org.wordpress.aztec.spans.AztecUnorderedListSpan
+import org.wordpress.aztec.spans.AztecVideoSpan
+import org.wordpress.aztec.spans.HiddenHtmlSpan
+import org.wordpress.aztec.spans.IAztecBlockSpan
+import org.wordpress.aztec.spans.ParagraphSpan
 import org.wordpress.aztec.util.getLast
 import org.xml.sax.Attributes
-import java.util.*
+import java.util.ArrayList
 
 class AztecTagHandler(val context: Context, val plugins: List<IAztecPlugin> = ArrayList()) : Html.TagHandler {
 
@@ -124,7 +139,7 @@ class AztecTagHandler(val context: Context, val plugins: List<IAztecPlugin> = Ar
         return false
     }
 
-    private fun processTagHandlerPlugins(tag: String, opening: Boolean, output: Editable, attributes: Attributes, nestingLevel: Int) : Boolean {
+    private fun processTagHandlerPlugins(tag: String, opening: Boolean, output: Editable, attributes: Attributes, nestingLevel: Int): Boolean {
         plugins.filter { it is IHtmlTagHandler }
                 .map { it as IHtmlTagHandler }
                 .forEach({
@@ -183,8 +198,7 @@ class AztecTagHandler(val context: Context, val plugins: List<IAztecPlugin> = Ar
 
         if (start != end) {
             output.setSpan(last, start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        }
-        else if (start == end && IAztecBlockSpan::class.java.isAssignableFrom(kind)) {
+        } else if (start == end && IAztecBlockSpan::class.java.isAssignableFrom(kind)) {
             //if block element is empty add a ZWJ to make it non empty and extend span
             output.append(Constants.ZWJ_CHAR)
             output.setSpan(last, start, output.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)

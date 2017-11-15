227e7713f6bd6d94a9ee4b7fd40e869173d345e3
Ondrej Ruttkay <ondrej@automattic.com>
2017-11-15 13:20:42 +0100

    Handle newlines around captions

--- wordpress-shortcodes/src/main/java/org/wordpress/aztec/plugins/shortcodes/handlers/CaptionHandler.kt
+++ wordpress-shortcodes/src/main/java/org/wordpress/aztec/plugins/shortcodes/handlers/CaptionHandler.kt
@@ -1,14 +1,40 @@
 package org.wordpress.aztec.plugins.shortcodes.handlers
 
+import org.wordpress.aztec.handlers.BlockHandler
 import org.wordpress.aztec.handlers.GenericBlockHandler
 import org.wordpress.aztec.plugins.shortcodes.spans.CaptionShortcodeSpan
+import org.wordpress.aztec.watchers.TextDeleter
 
-class CaptionHandler : GenericBlockHandler<CaptionShortcodeSpan>(CaptionShortcodeSpan::class.java) {
+class CaptionHandler : BlockHandler<CaptionShortcodeSpan>(CaptionShortcodeSpan::class.java) {
     override fun handleNewlineInBody() {
         block.end = newlineIndex + 1
     }
 
-    override fun handleNewlineAtTextEnd() {
-        block.end = newlineIndex + 1
+    override fun handleNewlineAtEmptyLineAtBlockEnd() {
+        // just remote the block since it's empty
+        block.remove()
+
+        // delete the newline as it's purpose was served (to translate it as a command to close the block)
+        TextDeleter.mark(text, newlineIndex, newlineIndex + 1)
     }
+
+    override fun handleEndOfBufferMarker() {
+        if (block.start == markerIndex) {
+            // ok, this list item has the marker as its first char so, nothing more to do here.
+            return
+        }
+
+        // the heading has bled over to the marker so, let's adjust its range to just before the marker.
+        //  There's a newline there hopefully :)
+        block.end = markerIndex
+    }
+
+    override fun handleNewlineAtEmptyBody() {
+        // just remote the block since it's empty
+        block.remove()
+
+        // delete the newline as it's purpose was served (to translate it as a command to close the block)
+        TextDeleter.mark(text, newlineIndex, newlineIndex + 1)
+    }
+
 }

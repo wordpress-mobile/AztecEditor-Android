59bff1133ab1036f20f09161b1c873ebfcb8276e
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2017-11-27 14:32:25 +0900

    Fixed scaling of images with ration < 1

--- glide-loader/src/main/java/org/wordpress/aztec/glideloader/extensions/BitmapExtension.kt
+++ glide-loader/src/main/java/org/wordpress/aztec/glideloader/extensions/BitmapExtension.kt
@@ -4,7 +4,9 @@ import android.graphics.Bitmap
 import android.graphics.Bitmap.createScaledBitmap
 
 internal fun Bitmap.upscaleTo(desiredWidth: Int): Bitmap {
-    val ratio = this.height / this.width
-    val height = (ratio * desiredWidth)
-    return createScaledBitmap(this, desiredWidth, height, true)
+    val ratio = this.height.toFloat() / this.width.toFloat()
+    val proportionateHeight = ratio * desiredWidth
+    val finalHeight = Math.rint(proportionateHeight.toDouble()).toInt()
+
+    return createScaledBitmap(this, desiredWidth, finalHeight, true)
 }

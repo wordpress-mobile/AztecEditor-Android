4a3787d97dd2d07c121eb186263428df1c2ad790
Ondrej Ruttkay <ondrej@automattic.com>
2017-11-29 13:43:22 +0100

    Use the regular activity for paragraph-testing

--- app/src/androidTest/kotlin/org/wordpress/aztec/demo/tests/ParagraphFormattingTests.kt
+++ app/src/androidTest/kotlin/org/wordpress/aztec/demo/tests/ParagraphFormattingTests.kt
@@ -4,14 +4,14 @@ import android.support.test.rule.ActivityTestRule
 import org.junit.Rule
 import org.junit.Test
 import org.wordpress.aztec.demo.BaseTest
-import org.wordpress.aztec.demo.NonCalypsoActivity
+import org.wordpress.aztec.demo.MainActivity
 import org.wordpress.aztec.demo.pages.EditorPage
 
-class NonCalypsoFormattingTests : BaseTest() {
+class ParagraphFormattingTests : BaseTest() {
 
     @Rule
     @JvmField
-    var mActivityTestRule = ActivityTestRule(NonCalypsoActivity::class.java)
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
 
     @Test
     fun testRetainParagraphFormatting() {

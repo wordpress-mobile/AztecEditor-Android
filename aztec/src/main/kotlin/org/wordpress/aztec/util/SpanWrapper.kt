f75896a59122399401843051ea4670b9cfdec218
Danilo Ercoli <ercoli@gmail.com>
2017-11-30 16:01:27 +0100

    Introduce a utility method that it's used in `start`, `end`, `flags` to set the span and log in case of errors

--- aztec/src/main/kotlin/org/wordpress/aztec/util/SpanWrapper.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/util/SpanWrapper.kt
@@ -17,33 +17,37 @@ class SpanWrapper<T>(var spannable: Spannable, var span: T) {
 
     var start: Int
         get() { return spannable.getSpanStart(span) }
-        set(start) { spannable.setSpan(span, start, end, flags) }
+        set(start) { setSpanOrLogError(span, start, end, flags) }
 
     var end: Int
         get() { return spannable.getSpanEnd(span) }
-        set(end) { spannable.setSpan(span, start, end, flags) }
+        set(end) { setSpanOrLogError(span, start, end, flags) }
 
     var flags: Int
         get() { return spannable.getSpanFlags(span) }
         set(flags) {
-            // Silently ignore invalid PARAGRAPH spans that don't start or end at paragraph boundary
-            // Copied from SpannableStringBuilder that throws an exception in this case.
-            val flagsStart = flags and START_MASK shr START_SHIFT
-            if (isInvalidParagraph(start, flagsStart)) {
-                AppLog.w(AppLog.T.EDITOR, "PARAGRAPH span must start at paragraph boundary"
-                        + " (" + start + " follows " + spannable.get(start - 1) + ")")
-                return
-            }
-
-            val flagsEnd = flags and END_MASK
-            if (isInvalidParagraph(end, flagsEnd)) {
-                AppLog.w(AppLog.T.EDITOR, "PARAGRAPH span must end at paragraph boundary"
-                        + " (" + end + " follows " + spannable.get(end - 1) + ")")
-                return
-            }
-
-            spannable.setSpan(span, start, end, flags)
+            setSpanOrLogError(span, start, end, flags)
         }
+
+    private fun setSpanOrLogError(span :T, start :Int, end :Int, flags :Int) {
+        // Silently ignore invalid PARAGRAPH spans that don't start or end at paragraph boundary
+        // Copied from SpannableStringBuilder that throws an exception in this case.
+        val flagsStart = flags and START_MASK shr START_SHIFT
+        if (isInvalidParagraph(start, flagsStart)) {
+            AppLog.w(AppLog.T.EDITOR, "PARAGRAPH span must start at paragraph boundary"
+                    + " (" + start + " follows " + spannable.get(start - 1) + ")")
+            return
+        }
+
+        val flagsEnd = flags and END_MASK
+        if (isInvalidParagraph(end, flagsEnd)) {
+            AppLog.w(AppLog.T.EDITOR, "PARAGRAPH span must end at paragraph boundary"
+                    + " (" + end + " follows " + spannable.get(end - 1) + ")")
+            return
+        }
+
+        spannable.setSpan(span, start, end, flags)
+    }
 
     private fun isInvalidParagraph(index: Int, flag: Int): Boolean {
         return flag == PARAGRAPH && index != 0 && index != spannable.length && spannable.get(index - 1) != '\n'

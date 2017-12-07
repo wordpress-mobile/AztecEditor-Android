86e4b0cc49852d192d5c0525460b95d599a0bd9b
Klymentiy Haykov <klymentiy.haykov@automattic.com>
2017-12-07 13:31:08 +0900

    Media toolbar buttons.

---
+++ app/src/main/kotlin/org/wordpress/aztec/demo/MediaPhotoToolbarButton.kt
@@ -0,0 +1,39 @@
+package org.wordpress.aztec.demo
+
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.wordpress.aztec.plugins.IMediaToolbarButton
+import org.wordpress.aztec.toolbar.AztecToolbar
+import org.wordpress.aztec.toolbar.IToolbarAction
+
+class MediaPhotoToolbarButton(val toolbar: AztecToolbar) : IMediaToolbarButton {
+
+    var clickListener: IMediaToolbarButton.IMediaToolbarClickListener? = null
+
+    override val action: IToolbarAction = MediaToolbarAction.PHOTO
+    override val context = toolbar.context!!
+
+    override fun setMediaToolbarButtonClickListener(clickListener: IMediaToolbarButton.IMediaToolbarClickListener) {
+        this.clickListener = clickListener
+    }
+
+    override fun toggle() {
+        if (clickListener != null){
+            clickListener!!.onClick(toolbar.findViewById(action.buttonId))
+        }
+    }
+
+    override fun matchesKeyShortcut(keyCode: Int, event: KeyEvent): Boolean {
+        return false
+    }
+
+    override fun inflateButton(parent: ViewGroup) {
+        LayoutInflater.from(context).inflate(R.layout.media_toobar_photo_button, parent)
+    }
+
+    override fun toolbarStateAboutToChange(toolbar: AztecToolbar, enable: Boolean) {
+        toolbar.findViewById<View>(action.buttonId).isEnabled = enable
+    }
+}

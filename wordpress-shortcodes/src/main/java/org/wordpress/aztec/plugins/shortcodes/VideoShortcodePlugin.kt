286327c66ba750f455268d2da762e5a9a6214ed7
Danilo Ercoli <ercoli@gmail.com>
2017-12-13 19:50:43 +0100

    Added the fuctions used to convert from videopress shortcode to HTML and viceversa

--- wordpress-shortcodes/src/main/java/org/wordpress/aztec/plugins/shortcodes/VideoShortcodePlugin.kt
+++ wordpress-shortcodes/src/main/java/org/wordpress/aztec/plugins/shortcodes/VideoShortcodePlugin.kt
@@ -6,14 +6,69 @@ import org.wordpress.aztec.plugins.visual2html.IHtmlPostprocessor
 class VideoShortcodePlugin : IHtmlPreprocessor, IHtmlPostprocessor {
 
     private val TAG = "video"
+    private val TAG_VIDEOPRESS_SHORTCODE = "wpvideo"
+    private val TAG_VIDEOPRESS_INNER_ID = "videopress_inner_id"
+
+    private fun fromVidePressShortCodeToHTML(source: MatchResult): String {
+        val match = source.groupValues.get(1)
+        val splittedMatch = match.split(" ")
+        var attributesBuilder = StringBuilder()
+        attributesBuilder.append("<$TAG ")
+
+
+        splittedMatch.forEach {
+            if (!it.isBlank()) {
+                if (it.contains('=')) {
+                    attributesBuilder.append(it)
+                } else {
+                    // This is the videopress ID
+                    attributesBuilder.append("$TAG_VIDEOPRESS_INNER_ID=" + it)
+                }
+                attributesBuilder.append(' ')
+            }
+        }
+
+        attributesBuilder.append(" />")
+        return attributesBuilder.toString()
+    }
 
     override fun beforeHtmlProcessed(source: String): String {
-        return source.replace(Regex("(?<!\\[)\\[$TAG([^\\]]*)\\](?!\\])"), "<$TAG$1 />")
+        var newSource = source.replace(Regex("(?<!\\[)\\[$TAG([^\\]]*)\\](?!\\])"), "<$TAG$1 />")
+        newSource = newSource.replace(Regex("(?<!\\[)\\[$TAG_VIDEOPRESS_SHORTCODE([^\\]]*)\\](?!\\])"), { it -> fromVidePressShortCodeToHTML(it) })
+        return newSource
+    }
+
+    private fun fromHTMLToShortcode(source: MatchResult): String {
+        val match = source.groupValues.get(1)
+        val splittedMatch = match.split(" ")
+        var attributesBuilder = StringBuilder()
+
+        var isVideoPress = false
+        splittedMatch.forEach {
+            if (!it.isBlank()) {
+                if (it.contains(TAG_VIDEOPRESS_INNER_ID)) {
+                    // This is the videopress ID attribute
+                    val splitted = it.split("=")
+                    if (splitted.size == 2) {
+                        // just make sure there is a correct ID
+                        attributesBuilder.append(splitted[1].replace("\"", ""))
+                        isVideoPress = true
+                    }
+                } else {
+                    attributesBuilder.append(it)
+                }
+                attributesBuilder.append(' ')
+            }
+        }
+
+        val shotcodeTag = if (isVideoPress) TAG_VIDEOPRESS_SHORTCODE else TAG
+        return "[$shotcodeTag " + attributesBuilder.toString().trim() + "]"
     }
 
     override fun onHtmlProcessed(source: String): String {
-        return StringBuilder(source)
-                .replace(Regex("<$TAG([^>]*(?<! )) */>"), "[$TAG$1]")
-                .replace(Regex("<$TAG([^>]*(?<! )) *></$TAG>"), "[$TAG$1]")
+        var newSource = StringBuilder(source)
+                .replace(Regex("<$TAG([^>]*(?<! )) */>"), { it -> fromHTMLToShortcode(it) })
+                .replace(Regex("<$TAG([^>]*(?<! )) */>"), { it -> fromHTMLToShortcode(it) })
+        return newSource
     }
 }

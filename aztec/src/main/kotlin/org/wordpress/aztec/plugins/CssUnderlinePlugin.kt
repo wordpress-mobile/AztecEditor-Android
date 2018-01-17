ffd06475eca46004b5264bc1034a21de84a4dab4
Ondrej Ruttkay <ondrej@automattic.com>
2018-01-17 22:30:20 +0100

    Fix the CSS underline span parsing and nesting

--- aztec/src/main/kotlin/org/wordpress/aztec/plugins/CssUnderlinePlugin.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/plugins/CssUnderlinePlugin.kt
@@ -2,12 +2,13 @@ package org.wordpress.aztec.plugins
 
 import android.text.SpannableStringBuilder
 import android.text.Spanned
-import org.wordpress.aztec.AztecAttributes
 import org.wordpress.aztec.plugins.html2visual.ISpanPostprocessor
 import org.wordpress.aztec.plugins.visual2html.ISpanPreprocessor
 import org.wordpress.aztec.source.InlineCssStyleFormatter
 import org.wordpress.aztec.spans.AztecUnderlineSpan
 import org.wordpress.aztec.spans.HiddenHtmlSpan
+import org.wordpress.aztec.spans.IAztecNestable
+import org.wordpress.aztec.util.SpanWrapper
 
 class CssUnderlinePlugin : ISpanPostprocessor, ISpanPreprocessor {
 
@@ -18,9 +19,28 @@ class CssUnderlinePlugin : ISpanPostprocessor, ISpanPreprocessor {
         spannable.getSpans(0, spannable.length, AztecUnderlineSpan::class.java).forEach {
             InlineCssStyleFormatter.addStyleAttribute(it.attributes, InlineCssStyleFormatter.CSS_TEXT_DECORATION_ATTRIBUTE, UNDERLINE_STYLE_VALUE)
 
-            val calypsoUnderlineSpan = HiddenHtmlSpan(SPAN_TAG, it.attributes, 0)
-            spannable.setSpan(calypsoUnderlineSpan, spannable.getSpanStart(it), spannable.getSpanEnd(it), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            val start = spannable.getSpanStart(it)
+            val nesting = IAztecNestable.getNestingLevelAt(spannable, start) + 1
+
+            val calypsoUnderlineSpan = HiddenHtmlSpan(SPAN_TAG, it.attributes, nesting)
+
+            spannable.setSpan(calypsoUnderlineSpan, start, spannable.getSpanEnd(it), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
             spannable.removeSpan(it)
+
+            // if a parent of the new hidden span is also a <span> we can merge them
+            IAztecNestable.getParent(spannable, SpanWrapper(spannable, calypsoUnderlineSpan))?.let {
+                if (it.span is HiddenHtmlSpan) {
+                    val hiddenSpan = it.span as HiddenHtmlSpan
+                    if (hiddenSpan.TAG == SPAN_TAG) {
+                        val parentStyle = hiddenSpan.attributes.getValue(InlineCssStyleFormatter.STYLE_ATTRIBUTE)
+                        val childStyle = calypsoUnderlineSpan.attributes.getValue(InlineCssStyleFormatter.STYLE_ATTRIBUTE)
+                        hiddenSpan.attributes.setValue(InlineCssStyleFormatter.STYLE_ATTRIBUTE, InlineCssStyleFormatter.mergeStyleAttributes(parentStyle, childStyle))
+
+                        // remove the extra child span
+                        spannable.removeSpan(calypsoUnderlineSpan)
+                    }
+                }
+            }
         }
     }
 

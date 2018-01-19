d82694f502d282e691583f67a9c488c2f8eb5130
Mario Zorz <mariozorz@gmail.com>
2018-01-19 16:22:45 -0300

    changed method isUserOperationPartiallyObservedInSequence to be implemented directly by superclass UserOperationEvent as realised implementation is the same across sequence models

--- aztec/src/main/kotlin/org/wordpress/aztec/watchers/event/sequence/known/space/API26InWordSpaceInsertionEvent.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/watchers/event/sequence/known/space/API26InWordSpaceInsertionEvent.kt
@@ -77,37 +77,6 @@ class API26InWordSpaceInsertionEvent : UserOperationEvent() {
         return false
     }
 
-    override fun isUserOperationPartiallyObservedInSequence(sequence: EventSequence<TextWatcherEvent>): Boolean {
-        for (i in sequence.indices) {
-
-            val eventHolder = this.sequence[i]
-            val observableEvent = sequence[i]
-
-            // if time distance between any of the events is longer than 50 millis, discard this as this pattern is
-            // likely not produced by the platform, but rather the user.
-            // WARNING! When debugging with breakpoints, you should disable this check as time can exceed the 50 MS limit and
-            // create undesired behavior.
-            if (i > 0) { // only try to compare when we have at least 2 events, so we can compare with the previous one
-                val timestampForPreviousEvent = sequence[i - 1].timestamp
-                val timeDistance = observableEvent.timestamp - timestampForPreviousEvent
-                if (timeDistance > ObservationQueue.MAXIMUM_TIME_BETWEEN_EVENTS_IN_PATTERN_MS) {
-                    return false
-                }
-            }
-
-            eventHolder.beforeEventData = observableEvent.beforeEventData
-            eventHolder.onEventData = observableEvent.onEventData
-            eventHolder.afterEventData = observableEvent.afterEventData
-
-            // return immediately as soon as we realize the pattern diverges
-            if (!eventHolder.testFitsBeforeOnAndAfter()) {
-                return false
-            }
-        }
-
-        return true
-    }
-
     override fun buildReplacementEventWithSequenceData(sequence: EventSequence<TextWatcherEvent>): TextWatcherEvent {
         val builder = TextWatcherEventInsertText.Builder()
         // here make it all up as a unique event that does the insert as usual, as we'd get it on older APIs

105202d41d8cede6827aba26864020f429392d33
Danilo Ercoli <ercoli@gmail.com>
2018-02-09 09:43:46 +0100

    Bring back the check if "atTextEnd" in HeadingHandler since moving it to the BlockHandler introduces many issues with lists.

--- aztec/src/main/kotlin/org/wordpress/aztec/handlers/BlockHandler.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/handlers/BlockHandler.kt
@@ -103,10 +103,6 @@ abstract class BlockHandler<SpanType : IAztecBlockSpan>(val clazz: Class<SpanTyp
             return PositionType.BUFFER_END
         }
 
-        if (atEndOfBlock) {
-            return PositionType.BUFFER_END
-        }
-
         // no special case applied so, newline is in the "body" of the block
         return PositionType.BODY
     }
--- aztec/src/main/kotlin/org/wordpress/aztec/handlers/HeadingHandler.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/handlers/HeadingHandler.kt
@@ -1,6 +1,7 @@
 package org.wordpress.aztec.handlers
 
 import android.text.Spannable
+import org.wordpress.aztec.Constants
 import org.wordpress.aztec.spans.AztecHeadingSpan
 import org.wordpress.aztec.watchers.TextDeleter
 
@@ -26,13 +27,25 @@ class HeadingHandler : BlockHandler<AztecHeadingSpan>(AztecHeadingSpan::class.ja
         TextDeleter.mark(text, newlineIndex, newlineIndex + 1)
     }
 
-    override fun handleNewlineAtTextEnd() {
-        block.end = newlineIndex + 1
-    }
+    // fun handleNewlineAtTextEnd()
+    // got a newline while being at the end-of-text. We'll let the current list item engulf it and will wait
+    //  for the end-of-text marker event in order to attach the new list item to it when that happens.
 
     override fun handleNewlineInBody() {
-        // newline added at some position inside the block. Let's split the block into two
-        cloneHeading(text, block.span, newlineIndex + 1, block.end)
+        // if the new newline is the second-last character of the block and the last one is a newline (which
+        // is a visual newline) or the end-of-buffer marker, or it's the last character of the text then it's the last
+        // actual character of the block
+        val atEndOfBlock = (newlineIndex == block.end - 2 &&
+                (text[block.end - 1] == Constants.NEWLINE || text[block.end - 1] == Constants.END_OF_BUFFER_MARKER)) ||
+                newlineIndex == text.length - 1
+
+        if (atEndOfBlock) {
+            // newline added at the end of the block (right before its visual newline) so, just end the block and
+            //  not add a new block after it
+        } else {
+            // newline added at some position inside the block. Let's split the block into two
+            cloneHeading(text, block.span, newlineIndex + 1, block.end)
+        }
 
         block.end = newlineIndex + 1
     }

d21ffd13de06b6a7565ce238d24452cb7e3d1173
Nick Bradbury <nick.bradbury@gmail.com>
2018-03-08 14:31:36 -0500

    Added drawables

--- app/src/main/kotlin/org/wordpress/aztec/demo/MediaToolbarAction.kt
+++ app/src/main/kotlin/org/wordpress/aztec/demo/MediaToolbarAction.kt
@@ -8,5 +8,6 @@ import org.wordpress.aztec.toolbar.ToolbarActionType
 enum class MediaToolbarAction constructor(override val buttonId: Int, override val actionType: ToolbarActionType,
                                           override val textFormat: ITextFormat) : IToolbarAction {
     GALLERY(R.id.media_bar_button_gallery, ToolbarActionType.OTHER, AztecTextFormat.FORMAT_NONE),
-    CAMERA(R.id.media_bar_button_camera, ToolbarActionType.OTHER, AztecTextFormat.FORMAT_NONE)
+    CAMERA(R.id.media_bar_button_camera, ToolbarActionType.OTHER, AztecTextFormat.FORMAT_NONE),
+    MORE(R.id.media_bar_button_more, ToolbarActionType.OTHER, AztecTextFormat.FORMAT_NONE);
 }

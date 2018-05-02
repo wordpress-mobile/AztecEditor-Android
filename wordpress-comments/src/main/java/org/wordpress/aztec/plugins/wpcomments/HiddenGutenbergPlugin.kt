ee3fbe3d4d8dcd792831010ac8ffeaa0e227d000
Mario Zorz <mariozorz@gmail.com>
2018-05-02 19:01:28 -0300

    modified slightly to include Gutenberg ending delimiter handling for code blocks

--- wordpress-comments/src/main/java/org/wordpress/aztec/plugins/wpcomments/HiddenGutenbergPlugin.kt
+++ wordpress-comments/src/main/java/org/wordpress/aztec/plugins/wpcomments/HiddenGutenbergPlugin.kt
@@ -37,7 +37,43 @@ class HiddenGutenbergPlugin : IHtmlCommentHandler, IInlineSpanHandler {
 
     override fun handleSpanStart(html: StringBuilder, span: CharacterStyle) {
         val gutenbergSpan = span as GutenbergCommentSpan
+        // Note on special case handling for closing </pre> (preformatted) spans:
+        // Gutenberg expects to find nothing there between the closing </pre> tag and its proper block end delimiter.
+        // Therefore, we need to check here whether this was a preformatted block, and shift <br> position if present,
+        // so the ending Gutenberg delimiter is just right after the </pre> ending tag
+        // check whether we're just about to process the Gutenberg block ending tag.
+        //
+        // For example, feed the following code into Aztec:
+        //        <!-- wp:preformatted -->
+        //        <pre class=\"wp-block-preformatted\">bla bla bla</pre>
+        //        <!-- /wp:preformatted -->
+        //
+        // In visual editor, placing the cursor at the end and hitting ENTER twice to exit the block would end up
+        // with this code:
+        //        <!-- wp:preformatted -->
+        //        <pre class=\"wp-block-preformatted\">bla bla bla</pre><br>
+        //        <!-- /wp:preformatted -->
+        // Note the misplaced <br> tag, which Gutenberg expects to be after the block end delimiter.
+        //
+        // As another example, same thing happens with Gutenberg Code blocks:
+        // <!-- wp:code -->
+        // <pre class="wp-block-code"><code> javascript code here </code></pre>
+        // <!-- /wp:code -->
+        var foundBreakAfterClosingPreTag = -1
+        if (gutenbergSpan.content.trimStart().startsWith("/wp:")) {
+            // now, strip the <br> after the last HTML closing </pre> tag if such a thing is found
+            foundBreakAfterClosingPreTag = html.lastIndexOf("</pre><br>")
+            if (foundBreakAfterClosingPreTag > -1) {
+                html.delete(foundBreakAfterClosingPreTag + 6, foundBreakAfterClosingPreTag + 6 + 4)
+            }
+        }
+
         html.append("<!--${gutenbergSpan.content}-->")
+
+        if (foundBreakAfterClosingPreTag > -1) {
+            // re-attach the <br> tag
+            html.append("<br>")
+        }
     }
 
     override fun handleSpanEnd(html: StringBuilder, span: CharacterStyle) {

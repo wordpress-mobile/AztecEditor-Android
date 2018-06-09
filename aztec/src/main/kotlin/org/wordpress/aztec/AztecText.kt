d6779c3ce39f8f0f298b66d0f10c9bc3ea915fe4
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-06-10 02:37:09 +0300

    Remove space as per ktlint

--- aztec/src/main/kotlin/org/wordpress/aztec/AztecText.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/AztecText.kt
@@ -871,7 +871,7 @@ open class AztecText : AppCompatEditText, TextWatcher, UnknownHtmlSpan.OnUnknown
             AztecTextFormat.FORMAT_STRIKETHROUGH,
             AztecTextFormat.FORMAT_CODE -> inlineFormatter.toggle(textFormat)
             AztecTextFormat.FORMAT_BOLD,
-            AztecTextFormat.FORMAT_STRONG  -> inlineFormatter.toggleAny(ToolbarAction.BOLD.textFormats)
+            AztecTextFormat.FORMAT_STRONG -> inlineFormatter.toggleAny(ToolbarAction.BOLD.textFormats)
             AztecTextFormat.FORMAT_UNORDERED_LIST -> blockFormatter.toggleUnorderedList()
             AztecTextFormat.FORMAT_ORDERED_LIST -> blockFormatter.toggleOrderedList()
             AztecTextFormat.FORMAT_ALIGN_LEFT,

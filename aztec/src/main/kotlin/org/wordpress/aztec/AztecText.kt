f77e57643e28eaa11c994b2de071abd1fe429cb7
Mario Zorz <mariozorz@gmail.com>
2019-05-02 09:17:17 -0300

    avoid running crash preventer InputFilter for newline insertion

--- aztec/src/main/kotlin/org/wordpress/aztec/AztecText.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/AztecText.kt
@@ -462,7 +462,7 @@ open class AztecText : AppCompatEditText, TextWatcher, UnknownHtmlSpan.OnUnknown
         // https://android-review.googlesource.com/c/platform/frameworks/base/+/634929
         val dynamicLayoutCrashPreventer = InputFilter { source, start, end, dest, dstart, dend ->
             var temp : CharSequence? = null
-            if (!bypassCrashPreventerInputFilter && dend < dest.length) {
+            if (!bypassCrashPreventerInputFilter && dend < dest.length && source != Constants.NEWLINE_STRING) {
 
                 // if there are any images right after the destination position, hack the text
                 val spans = dest.getSpans(dend, dend+1, AztecImageSpan::class.java)

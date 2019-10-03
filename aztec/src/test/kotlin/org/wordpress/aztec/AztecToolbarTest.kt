29bcf1320c39d29f513b087302c65653586d7cea
Marko Savic <savicmarko1985@gmail.com>
2019-10-03 11:45:14 +0200

    Fixed tests

--- aztec/src/test/kotlin/org/wordpress/aztec/AztecToolbarTest.kt
+++ aztec/src/test/kotlin/org/wordpress/aztec/AztecToolbarTest.kt
@@ -648,7 +648,7 @@ class AztecToolbarTest {
     @Throws(Exception::class)
     fun emptySelection() {
         editText.fromHtml("<b>bold</b><b><i>italic</i></b>")
-        editText.fromHtml("")
+        editText.setText("")
 
         Assert.assertTrue(TestUtils.safeEmpty(editText))
 

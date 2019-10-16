23f6d3b02f9c74207c3590cfc571667fda25b42f
Danilo Ercoli <ercoli@gmail.com>
2019-10-16 15:41:54 +0200

    Fix lint

--- aztec/src/main/kotlin/org/wordpress/aztec/util/DynamicLayoutGetBlockIndexOutOfBoundsException.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/util/DynamicLayoutGetBlockIndexOutOfBoundsException.kt
@@ -2,4 +2,4 @@ package org.wordpress.aztec.util
 
 import java.lang.RuntimeException
 
-class DynamicLayoutGetBlockIndexOutOfBoundsException(message:String, cause:Throwable): RuntimeException(message, cause)
+class DynamicLayoutGetBlockIndexOutOfBoundsException(message: String, cause: Throwable) : RuntimeException(message, cause)

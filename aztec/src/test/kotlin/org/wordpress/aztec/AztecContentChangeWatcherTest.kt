807062ab67edb46a777883aedc107fc80bb13659
vojtasmrcek <planarvoid@gmail.com>
2019-12-20 13:42:17 +0100

    Fix lint issue

--- aztec/src/test/kotlin/org/wordpress/aztec/AztecContentChangeWatcherTest.kt
+++ aztec/src/test/kotlin/org/wordpress/aztec/AztecContentChangeWatcherTest.kt
@@ -1,6 +1,7 @@
 package org.wordpress.aztec
 
-import org.junit.Assert.*
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith

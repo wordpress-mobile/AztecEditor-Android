650701b0f11092ce2bf1f958c048d80f2c9624f2
Klymentiy haykov <forsver@gmail.com>
2020-04-20 17:08:53 -0700

    Removed unused import.

--- aztec/src/main/kotlin/org/wordpress/aztec/toolbar/RippleToggleButton.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/toolbar/RippleToggleButton.kt
@@ -9,7 +9,6 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnLongClickListener
 import android.widget.Toast
-import android.widget.ToggleButton
 import androidx.appcompat.widget.AppCompatToggleButton
 import org.wordpress.aztec.R
 

5177683fd2a5b180125b6649a85fb76e10279d30
Cameron Voell <cameronvoell@gmail.com>
2020-07-20 16:00:32 -0700

    Update Aztec to use default strikethrough tag of <s> instead of <del>

--- aztec/src/main/kotlin/org/wordpress/aztec/spans/AztecStrikethroughSpan.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/spans/AztecStrikethroughSpan.kt
@@ -3,7 +3,7 @@ package org.wordpress.aztec.spans
 import android.text.style.StrikethroughSpan
 import org.wordpress.aztec.AztecAttributes
 
-class AztecStrikethroughSpan(tag: String = "del",
+class AztecStrikethroughSpan(tag: String = "s",
                              override var attributes: AztecAttributes = AztecAttributes())
     : StrikethroughSpan(), IAztecInlineSpan {
     override val TAG = tag

b58d9bdb37ce8ad1ac16c0920c2d3cc4f62a06b7
Yuval Gnessin <yuval.gnessin@quizlet.com>
2021-01-26 18:58:33 -0800

    lint: address ktlint concerns

--- aztec/src/main/kotlin/org/wordpress/aztec/plugins/BackgroundColorButton.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/plugins/BackgroundColorButton.kt
@@ -1,18 +1,15 @@
 package org.wordpress.aztec.plugins
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.wordpress.aztec.AztecText
-import org.wordpress.aztec.AztecTextFormat
 import org.wordpress.aztec.R
 import org.wordpress.aztec.toolbar.AztecToolbar
 import org.wordpress.aztec.toolbar.IToolbarAction
 import org.wordpress.aztec.toolbar.ToolbarAction
-import org.wordpress.aztec.toolbar.ToolbarActionType
 
-class BackgroundColorButton(val visualEditor: AztecText) : IToolbarButton {
+class BackgroundColorButton(private val visualEditor: AztecText) : IToolbarButton {
 
     override val action: IToolbarAction = ToolbarAction.BACKGROUND
     override val context = visualEditor.context!!

67515e66e0f04b61ddd902eeaff289f1d131d418
vojtasmrcek <planarvoid@gmail.com>
2022-02-18 10:15:19 +0100

    Move layout to ToolbarAction.kt and improve ToolbarItems.kt

--- aztec/src/main/kotlin/org/wordpress/aztec/toolbar/ToolbarItems.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/toolbar/ToolbarItems.kt
@@ -12,19 +12,16 @@ sealed class ToolbarItems {
      * This class defines the elements in the basic layout. It contains a set of visible items in a given order.
      * The plugins are added automatically at the end if they are missing from the toolbarElements object.
      */
-    data class BasicLayout(val toolbarItemGroup: ToolbarItemGroup) : ToolbarItems() {
-        constructor(vararg toolbarItems: ToolbarItem): this(toolbarItemGroup = ToolbarItemGroup(linkedSetOf(*toolbarItems)))
-        init {
-            if (!toolbarItemGroup.toolbarItems.contains(ToolbarItem.PLUGINS)) {
-                toolbarItemGroup.toolbarItems.add(ToolbarItem.PLUGINS)
-            }
-        }
+    class BasicLayout(vararg toolbarItems: IToolbarItem) : ToolbarItems() {
+        val sanitizedInput = toolbarItems.toList().sanitize(containsPlugins = true)
 
         /**
          * Draws toolbar elements into the given layout
          */
         fun addInto(toolbarContainer: LinearLayout, inflater: LayoutInflater) {
-            addItems(toolbarContainer, inflater, toolbarItemGroup)
+            sanitizedInput.forEachIndexed { index, toolbarElement ->
+                toolbarElement.addInto(toolbarContainer, inflater, index)
+            }
         }
     }
 
@@ -33,108 +30,115 @@ sealed class ToolbarItems {
      * Collapsed items are visible by default and the expanded items are visible on button click. The Plugins element
      * is added to the expanded items if missing. Plugins are never visible in the collapsed items.
      */
-    data class AdvancedLayout(val expandedItemGroup: ToolbarItemGroup, val collapsedItemGroup: ToolbarItemGroup) : ToolbarItems() {
-        init {
-            if (!expandedItemGroup.toolbarItems.contains(ToolbarItem.PLUGINS)) {
-                expandedItemGroup.toolbarItems.add(ToolbarItem.PLUGINS)
-            }
-            if (collapsedItemGroup.toolbarItems.contains(ToolbarItem.PLUGINS)) {
-                collapsedItemGroup.toolbarItems.remove(ToolbarItem.PLUGINS)
-            }
-        }
+    class AdvancedLayout(expandedItems: List<IToolbarItem>, collapsedItems: List<IToolbarItem>) : ToolbarItems() {
+        val sanitizedExpandedItems = expandedItems.sanitize(containsPlugins = true)
+        val sanitizedCollapsedItems = collapsedItems.sanitize(containsPlugins = false, listOfUsedItems = sanitizedExpandedItems)
 
         /**
          * Draws the expanded items into the expanded container and collapsed items into the collapsed container.
          */
         fun addInto(expandedContainer: LinearLayout, collapsedContainer: LinearLayout, inflater: LayoutInflater) {
-            addItems(expandedContainer, inflater, expandedItemGroup)
-            addItems(collapsedContainer, inflater, collapsedItemGroup)
+            sanitizedExpandedItems.forEachIndexed { index, toolbarElement ->
+                toolbarElement.addInto(expandedContainer, inflater, index)
+            }
+            sanitizedCollapsedItems.forEachIndexed { index, toolbarElement ->
+                toolbarElement.addInto(collapsedContainer, inflater, index)
+            }
         }
     }
 
     companion object {
-        fun addItems(container: LinearLayout, inflater: LayoutInflater, toolbarItemGroup: ToolbarItemGroup) {
-            var offset = 0
-            toolbarItemGroup.toolbarItems.forEachIndexed { index, toolbarElement ->
-                if (toolbarItemGroup.verticalDividerIndices.contains(index)) {
-                    val view = inflater.inflate(R.layout.format_bar_vertical_divider, null)
-                    container.addView(view, index + offset)
-                    offset += 1
-                }
-                toolbarElement.addInto(container, inflater, index + offset)
-            }
-        }
-
         /**
          * The default order of elements in the basic layout. It's used when there is no order added by the user.
          */
         val defaultBasicLayout = BasicLayout(
-                ToolbarItem.HEADING,
-                ToolbarItem.LIST,
-                ToolbarItem.QUOTE,
-                ToolbarItem.BOLD,
-                ToolbarItem.ITALIC,
-                ToolbarItem.LINK,
-                ToolbarItem.UNDERLINE,
-                ToolbarItem.STRIKETHROUGH,
-                ToolbarItem.ALIGN_LEFT,
-                ToolbarItem.ALIGN_CENTER,
-                ToolbarItem.ALIGN_RIGHT,
-                ToolbarItem.HORIZONTAL_RULE,
-                ToolbarItem.PLUGINS,
-                ToolbarItem.HTML
+                ToolbarAction.HEADING,
+                ToolbarAction.LIST,
+                ToolbarAction.QUOTE,
+                ToolbarAction.BOLD,
+                ToolbarAction.ITALIC,
+                ToolbarAction.LINK,
+                ToolbarAction.UNDERLINE,
+                ToolbarAction.STRIKETHROUGH,
+                ToolbarAction.ALIGN_LEFT,
+                ToolbarAction.ALIGN_CENTER,
+                ToolbarAction.ALIGN_RIGHT,
+                ToolbarAction.HORIZONTAL_RULE,
+                PLUGINS,
+                ToolbarAction.HTML
         )
 
         /**
          * The default order of elements in the advanced layout. It's used when there is no order added by the user.
          */
         val defaultAdvancedLayout = AdvancedLayout(
-                expandedItemGroup = ToolbarItemGroup(linkedSetOf(
-                        ToolbarItem.LINK,
-                        ToolbarItem.UNDERLINE,
-                        ToolbarItem.STRIKETHROUGH,
-                        ToolbarItem.ALIGN_LEFT,
-                        ToolbarItem.ALIGN_CENTER,
-                        ToolbarItem.ALIGN_RIGHT,
-                        ToolbarItem.HORIZONTAL_RULE,
-                        ToolbarItem.PLUGINS,
-                        ToolbarItem.HTML
-                )),
-                collapsedItemGroup = ToolbarItemGroup(linkedSetOf(
-                        ToolbarItem.HEADING,
-                        ToolbarItem.LIST,
-                        ToolbarItem.QUOTE,
-                        ToolbarItem.BOLD,
-                        ToolbarItem.ITALIC
-                )))
+                expandedItems = mutableListOf(
+                        ToolbarAction.LINK,
+                        ToolbarAction.UNDERLINE,
+                        ToolbarAction.STRIKETHROUGH,
+                        ToolbarAction.ALIGN_LEFT,
+                        ToolbarAction.ALIGN_CENTER,
+                        ToolbarAction.ALIGN_RIGHT,
+                        ToolbarAction.HORIZONTAL_RULE,
+                        PLUGINS,
+                        ToolbarAction.HTML
+                ),
+                collapsedItems = mutableListOf(
+                        ToolbarAction.HEADING,
+                        ToolbarAction.LIST,
+                        ToolbarAction.QUOTE,
+                        ToolbarAction.BOLD,
+                        ToolbarAction.ITALIC
+                ))
     }
-
-    data class ToolbarItemGroup(val toolbarItems: LinkedHashSet<ToolbarItem>, val verticalDividerIndices: Set<Int> = emptySet())
 
     /**
      * A list of supported default toolbar elements. If you need to add custom toolbar elements, create a new Plugin.
      */
-    enum class ToolbarItem(val layout: Int? = null) {
-        HEADING(R.layout.format_bar_button_heading),
-        LIST(R.layout.format_bar_button_list),
-        QUOTE(R.layout.format_bar_button_quote),
-        BOLD(R.layout.format_bar_button_bold),
-        ITALIC(R.layout.format_bar_button_italic),
-        LINK(R.layout.format_bar_button_link),
-        UNDERLINE(R.layout.format_bar_button_underline),
-        STRIKETHROUGH(R.layout.format_bar_button_strikethrough),
-        ALIGN_LEFT(R.layout.format_bar_button_align_left),
-        ALIGN_CENTER(R.layout.format_bar_button_align_center),
-        ALIGN_RIGHT(R.layout.format_bar_button_align_right),
-        HORIZONTAL_RULE(R.layout.format_bar_button_horizontal_line),
-        HTML(R.layout.format_bar_button_html),
-        PLUGINS;
+    fun IToolbarItem.addInto(toolbarContainer: LinearLayout, inflater: LayoutInflater, index: Int) {
+        this.layout?.let {
+            val view = inflater.inflate(it, null)
+            toolbarContainer.addView(view, index)
+        }
+    }
 
-        fun addInto(toolbarContainer: LinearLayout, inflater: LayoutInflater, index: Int) {
-            layout?.let {
-                val view = inflater.inflate(layout, null)
-                toolbarContainer.addView(view, index)
+    protected fun List<IToolbarItem>.sanitize(containsPlugins: Boolean, listOfUsedItems: List<IToolbarItem> = emptyList()): List<IToolbarItem> {
+        val usedToolbarActions = mutableSetOf<ToolbarAction>()
+        usedToolbarActions.addAll(listOfUsedItems.filterIsInstance(ToolbarAction::class.java).toSet())
+        val result = mutableListOf<IToolbarItem>()
+        var hasPlugins = false
+        for (toolbarItem in this) {
+            when (toolbarItem) {
+                is ToolbarAction -> {
+                    if (toolbarItem.layout != null && !usedToolbarActions.contains(toolbarItem)) {
+                        usedToolbarActions.add(toolbarItem)
+                        result.add(toolbarItem)
+                    }
+                }
+                is PLUGINS -> {
+                    if (!hasPlugins && containsPlugins) {
+                        hasPlugins = true
+                        result.add(toolbarItem)
+                    }
+                }
+                is DIVIDER -> {
+                    result.add(toolbarItem)
+                }
             }
         }
+        if (!hasPlugins && containsPlugins) {
+            result.add(PLUGINS)
+        }
+        return result
+    }
+
+    interface IToolbarItem {
+        val layout: Int?
+    }
+    object PLUGINS : IToolbarItem {
+        override val layout: Int? = null
+    }
+    object DIVIDER : IToolbarItem {
+        override val layout: Int = R.layout.format_bar_vertical_divider
     }
 }

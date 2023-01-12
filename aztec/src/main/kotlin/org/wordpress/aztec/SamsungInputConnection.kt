7d5e43d67c54e96ac8885afb38fb1042d87c7ff7
khaykov <forsver@gmail.com>
2023-01-12 13:08:57 -0600

    Add an additional handling for Grammarly events.

--- aztec/src/main/kotlin/org/wordpress/aztec/SamsungInputConnection.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/SamsungInputConnection.kt
@@ -1,23 +1,27 @@
 package org.wordpress.aztec
 
 import android.os.Build
-import android.widget.TextView
-import android.view.inputmethod.BaseInputConnection
+import android.os.Bundle
 import android.text.Editable
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.TextUtils
+import android.text.style.SuggestionSpan
+import android.view.KeyEvent
+import android.view.inputmethod.BaseInputConnection
 import android.view.inputmethod.CompletionInfo
 import android.view.inputmethod.CorrectionInfo
-import android.view.inputmethod.ExtractedTextRequest
 import android.view.inputmethod.ExtractedText
+import android.view.inputmethod.ExtractedTextRequest
 import android.view.inputmethod.InputConnection
-import android.os.Bundle
-import android.view.KeyEvent
 import android.view.inputmethod.InputContentInfo
+import org.wordpress.aztec.spans.IAztecSpan
 
 /**
  * Wrapper around proprietary Samsung InputConnection. Forwards all the calls to it, except for getExtractedText
  */
 class SamsungInputConnection(
-        private val mTextView: TextView,
+        private val mTextView: AztecText,
         private val baseInputConnection: InputConnection,
 ) : BaseInputConnection(mTextView, true) {
 
@@ -59,7 +63,7 @@ class SamsungInputConnection(
 
     // Extracted text on Samsung devices on Android 13 is somehow used for Grammarly suggestions which causes a lot of
     // issues with spans and cursors. We do not use extracted text, so returning null
-    // (default behavior of BaseInputConnection) fixes the problem
+    // (default behavior of BaseInputConnection) prevents Grammarly from messing up content most of the time
     override fun getExtractedText(request: ExtractedTextRequest?, flags: Int): ExtractedText? {
         return null
     }
@@ -73,6 +77,34 @@ class SamsungInputConnection(
     }
 
     override fun commitText(text: CharSequence?, newCursorPosition: Int): Boolean {
+        val isSameStringValue = text.toString() == editable.toString()
+        val incomingTextHasSuggestions = text is Spanned &&
+                text.getSpans(0, text.length, SuggestionSpan::class.java).isNotEmpty()
+
+        // Despite returning null from getExtractedText, in some cases Grammarly still tries to replace content of the
+        // editor with own content containing suggestions. This mostly works ok, but Aztec spans are finicky, and tend
+        // to get messed when content of the editor is replaced. In this method we remove Aztec spans before committing
+        // the change, and reapply them afterward
+        if (isSameStringValue && incomingTextHasSuggestions) {
+            // create a clean spannable string from editable to temporarily hold spans
+            val tempString = SpannableStringBuilder(editable.toString())
+
+            // store Aztec and Suggestions spans in temp string
+            TextUtils.copySpansFrom(editable, 0, editable.length, IAztecSpan::class.java, tempString, 0)
+            TextUtils.copySpansFrom(text as Spanned, 0, editable.length, SuggestionSpan::class.java, tempString, 0)
+
+            // remove all the Aztec spans from the current content of editor
+            editable.getSpans(0, editable.length, IAztecSpan::class.java).forEach { editable.removeSpan(it) }
+
+            // commit the text
+            val result = baseInputConnection.commitText(text, newCursorPosition)
+
+            // re-add the spans we removed before committing the text
+            TextUtils.copySpansFrom(tempString, 0, editable.length, IAztecSpan::class.java, editable, 0)
+            TextUtils.copySpansFrom(tempString, 0, editable.length, SuggestionSpan::class.java, editable, 0)
+
+            return result
+        }
         return baseInputConnection.commitText(text, newCursorPosition)
     }
 
